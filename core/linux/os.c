ptrbman/dynamorio

commit 19df14844bd9a236ab1cea59d0c759071099da70

fixes issue 939
+ Added DYNAMORIO_CONFIGDIR alternative to HOME as a config dir location.
  It takes precedence over HOME.
+ Additionally, drrun (but not drconfig) will use a temp dir rather than
  failing.  To accomplish this, I added a new drconfiglib API routine
  dr_get_config_dir() which must be called w/ appropriate params
  prior to creating the child process.
+ Propagate DYNAMORIO_CONFIGDIR across execve and Windows process creation.
  As part of this, re-factored the env var propagation in both linux/os.c
  and windows/syscall.c to make it easiser to add more env vars in the
  future.
+ Decided against adding a config_dir param to all of the drconfig
  API routines at this time.
+ Did not add a test b/c it's not easy to do so w/ our existing harness but
  I did manual tests on Linux and Windows for 2-step and 1-step configs.

git-svn-id: https://dynamorio.googlecode.com/svn/trunk@1661 49cc7528-f6fd-11dd-9d1a-b59b2e1864b6

core/linux/os.c
@@ -4556,6 +4556,24 @@ handle_self_signal(dcontext_t *dcontext, uint sig)
     }
 }
 
+/***************************************************************************
+ * EXECVE
+ */
+
+/* when adding here, also add to the switch in handle_execve if necessary */
+enum {
+    ENV_PROP_RUNUNDER,
+    ENV_PROP_OPTIONS,
+};
+static const char * const env_to_propagate[] = {
+    /* these must line up with the enum */
+    DYNAMORIO_VAR_RUNUNDER,
+    DYNAMORIO_VAR_OPTIONS,
+    /* un-named */
+    DYNAMORIO_VAR_CONFIGDIR,
+};
+#define NUM_ENV_TO_PROPAGATE (sizeof(env_to_propagate)/sizeof(env_to_propagate[0]))
+
 static void
 handle_execve(dcontext_t *dcontext)
 {
@@ -4584,7 +4602,9 @@ handle_execve(dcontext_t *dcontext)
      */
     char *fname = (char *)  sys_param(dcontext, 0);
     char **envp = (char **) sys_param(dcontext, 2);
-    int i, preload = -1, ldpath = -1, ops = -1, rununder = -1;
+    int i, j, preload = -1, ldpath = -1;
+    int prop_found[NUM_ENV_TO_PROPAGATE];
+    int prop_idx[NUM_ENV_TO_PROPAGATE];
     bool preload_us = false, ldpath_us = false;
     bool x64 = IF_X64_ELSE(true, false);
     file_t file;
@@ -4623,6 +4643,9 @@ handle_execve(dcontext_t *dcontext)
     inject_library_path = IF_X64_ELSE(x64, !x64) ? dynamorio_library_path :
         dynamorio_alt_arch_path;
 
+    for (j = 0; j < NUM_ENV_TO_PROPAGATE; j++)
+        prop_found[j] = -1;
+
     if (envp == NULL) {
         LOG(THREAD, LOG_SYSCALLS, 3, "\tenv is NULL\n");
         i = 0;
@@ -4630,11 +4653,11 @@ handle_execve(dcontext_t *dcontext)
         for (i = 0; envp[i] != NULL; i++) {
             /* execve env vars should never be set here */
             ASSERT(strstr(envp[i], DYNAMORIO_VAR_EXECVE) != envp[i]);
-            if (strstr(envp[i], DYNAMORIO_VAR_OPTIONS) == envp[i]) {
-                ops = i;
-            }
-            if (strstr(envp[i], DYNAMORIO_VAR_RUNUNDER) == envp[i]) {
-                rununder = i;
+            for (j = 0; j < NUM_ENV_TO_PROPAGATE; j++) {
+                if (strstr(envp[i], env_to_propagate[j]) == envp[i]) {
+                    prop_found[j] = i;
+                    break;
+                }
             }
             if (strstr(envp[i], "LD_LIBRARY_PATH=") == envp[i]) {
                 ldpath = i;
@@ -4666,9 +4689,7 @@ handle_execve(dcontext_t *dcontext)
     int num_old = i;
     uint sz;
     char *var, *old;
-    int idx_preload = preload, idx_ldpath = ldpath, idx_ops = ops;
-    int idx_rununder = rununder;
-    char *options = option_string; /* global var */
+    int idx_preload = preload, idx_ldpath = ldpath;
     int num_new;
     char **new_envp;
     uint logdir_length;
@@ -4696,10 +4717,11 @@ handle_execve(dcontext_t *dcontext)
         ((preload<0) ? 1 : 0) +
         ((ldpath<0) ? 1 : 0);
     if (DYNAMO_OPTION(follow_children)) {
-        num_new +=
-            ((rununder < 0) ? 1 : 0) +
-            ((ops < 0 && options != NULL) ? 1 : 0) +
-            (get_log_dir(PROCESS_DIR, NULL, NULL) ? 1 : 0) /* logdir */;
+        num_new += (get_log_dir(PROCESS_DIR, NULL, NULL) ? 1 : 0) /* logdir */;
+        for (j = 0; j < NUM_ENV_TO_PROPAGATE; j++) {
+            if (prop_found[j] < 0)
+                num_new++;
+        }
     }
     new_envp = heap_alloc(dcontext, sizeof(char*)*(num_old+num_new)
                           HEAPACCT(ACCT_OTHER));
@@ -4717,10 +4739,10 @@ handle_execve(dcontext_t *dcontext)
     if (ldpath < 0)
         idx_ldpath = i++;
     if (DYNAMO_OPTION(follow_children)) {
-        if (rununder < 0)
-            idx_rununder = i++;
-        if (ops < 0 && options != NULL)
-            idx_ops = i++;
+        for (j = 0; j < NUM_ENV_TO_PROPAGATE; j++) {
+            if (prop_found[j] < 0)
+                prop_idx[j] = i++;
+        }
     }
 
     if (!preload_us) {
@@ -4764,25 +4786,34 @@ handle_execve(dcontext_t *dcontext)
     }
 
     if (DYNAMO_OPTION(follow_children)) {
-        if (rununder < 0) {
-            sz = strlen(DYNAMORIO_VAR_RUNUNDER) + 3 /* =, 1, null */;
-            var = heap_alloc(dcontext, sizeof(char)*sz HEAPACCT(ACCT_OTHER));
-            /* Must pass RUNUNDER_ALL to get child injected if has no app config */
-            snprintf(var, sz, "%s=%.1d", DYNAMORIO_VAR_RUNUNDER,
-                     RUNUNDER_ON | RUNUNDER_ALL);
-            *(var+sz-1) = '\0'; /* null terminate */
-            new_envp[idx_rununder] = var;
-            LOG(THREAD, LOG_SYSCALLS, 2, "\tnew env %d: %s\n",
-                idx_rununder, new_envp[idx_rununder]);
-        } /* If rununder var is already set we assume it's set to 1. */
-        if (ops < 0 && options != NULL) {
-            sz = strlen(DYNAMORIO_VAR_OPTIONS) + strlen(options) + 2;
-            var = heap_alloc(dcontext, sizeof(char)*sz HEAPACCT(ACCT_OTHER));
-            snprintf(var, sz, "%s=%s", DYNAMORIO_VAR_OPTIONS, options);
-            *(var+sz-1) = '\0'; /* null terminate */
-            new_envp[idx_ops] = var;
-            LOG(THREAD, LOG_SYSCALLS, 2, "\tnew env %d: %s\n",
-                idx_ops, new_envp[idx_ops]);
+        for (j = 0; j < NUM_ENV_TO_PROPAGATE; j++) {
+            if (prop_found[j] < 0) {
+                const char *val = "";
+                switch (j) {
+                case ENV_PROP_RUNUNDER:
+                    ASSERT(strcmp(env_to_propagate[j], DYNAMORIO_VAR_RUNUNDER) == 0);
+                    /* Must pass RUNUNDER_ALL to get child injected if has no app config.
+                     * If rununder var is already set we assume it's set to 1.
+                     */
+                    ASSERT((RUNUNDER_ON | RUNUNDER_ALL) == 0x3); /* else, update "3" */
+                    val = "3";
+                    break;
+                case ENV_PROP_OPTIONS:
+                    ASSERT(strcmp(env_to_propagate[j], DYNAMORIO_VAR_OPTIONS) == 0);
+                    val = option_string;
+                    break;
+                default:
+                    val = getenv(env_to_propagate[j]);
+                    break;
+                }
+                sz = strlen(env_to_propagate[j]) + strlen(val) + 2 /* '=' + null */;
+                var = heap_alloc(dcontext, sizeof(char)*sz HEAPACCT(ACCT_OTHER));
+                snprintf(var, sz, "%s=%s", env_to_propagate[j], val);
+                *(var+sz-1) = '\0'; /* null terminate */
+                new_envp[prop_idx[j]] = var;
+                LOG(THREAD, LOG_SYSCALLS, 2, "\tnew env %d: %s\n",
+                    prop_idx[j], new_envp[prop_idx[j]]);
+            }
         }
         if (get_log_dir(PROCESS_DIR, NULL, &logdir_length)) {
             sz = strlen(DYNAMORIO_VAR_EXECVE_LOGDIR) + 1 + logdir_length;
@@ -4793,13 +4824,13 @@ handle_execve(dcontext_t *dcontext)
             new_envp[i++] = var;
             LOG(THREAD, LOG_SYSCALLS, 2, "\tnew env %d: %s\n", i-1, new_envp[i-1]);
         }
-    } else if (idx_rununder >= 0) {
+    } else if (prop_idx[ENV_PROP_RUNUNDER] >= 0) {
         /* disable auto-following of this execve, yet still allow preload
          * on other side to inject if config file exists.
          * kind of hacky mangle here:
          */
-        ASSERT(new_envp[idx_rununder][0] == 'D');
-        new_envp[idx_rununder][0] = 'X';
+        ASSERT(new_envp[prop_idx[ENV_PROP_RUNUNDER]][0] == 'D');
+        new_envp[prop_idx[ENV_PROP_RUNUNDER]][0] = 'X';
     }
 
     sz = strlen(DYNAMORIO_VAR_EXECVE) + 4;
@@ -4926,6 +4957,8 @@ handle_close_pre(dcontext_t *dcontext)
     }
     return true;
 }
+
+/***************************************************************************/
 
 /* Used to obtain the pc of the syscall instr itself when the dcontext dc
  * is currently in a syscall handler.

api/docs/intro.dox
@@ -367,7 +367,12 @@ processes.
 Configuration information is stored in files in the current user's profile
 directory, which is obtained from the environment variable \c $USERPROFILE.
 Thus, configurations are persistent across reboots and are private to each
-user.  DynamoRIO also supports global configurations, which are stored in
+user.  An alternative to <tt>$USERPROFILE</tt> can be
+specified by setting the <tt>DYNAMORIO_CONFIGDIR</tt> environment variable;
+if neither is set, a temp directory will be used when creating new
+configuration files for configure-and-run execution.
+
+DynamoRIO also supports global configurations, which are stored in
 the "config" subdirectory of the directory specified by the \c
 DYNAMORIO_HOME registry value in the registry key \c
 \\HKLM\\SOFTWARE\\DynamoRIO\\DynamoRIO (or for 32-bit on 64-bit Windows
@@ -505,7 +510,12 @@ invoke the parent process.  The \p drrun script can also be used but it
 creates a temporary configuration file that will override settings
 requested via \p drconfig.  The configuration file for each application is
 stored in <tt>$HOME/.dynamorio/&lt;appname&gt;.config32</tt> (or a \p
-config64 suffix for 64-bit).  DynamoRIO also supports global configuration
+config64 suffix for 64-bit).  An alternative to <tt>$HOME</tt> can be
+specified by setting the <tt>DYNAMORIO_CONFIGDIR</tt> environment variable;
+if neither is set, a temp directory will be used when creating new
+configuration files for configure-and-run execution.
+
+DynamoRIO also supports global configuration
 files in <tt>/etc/dynamorio/&lt;appname&gt;.config32</tt> when a local
 configuration file is not found.  \p drconfig does not support directly
 writing a global config file but such files can be copied from or modeled

api/docs/release.dox
@@ -195,6 +195,8 @@ Further non-compatibility-affecting changes include:
  - Added a new operand type enabling an instr_t to be used as a
    memory reference: opnd_create_mem_instr(), opnd_is_mem_instr(),
    opnd_get_mem_instr_disp()
+ - Added DYNAMORIO_CONFIGDIR alternative to HOME as a config dir location.
+   Additionally, drrun uses a temp dir for config files rather than failing.
  - Deprecated dr_save_arith_flags() and dr_restore_arith_flags()
  - Fixed performance regression involving inlined clean calls
  - Fixed bug involving 32-bit Linux support on a 64-bit Linux

core/config.c
@@ -47,6 +47,7 @@
 
 #include <string.h>
 
+/* DYNAMORIO_VAR_CONFIGDIR is searched first, and then these: */
 #ifdef LINUX
 # define GLOBAL_CONFIG_DIR "/etc/dynamorio"
 # define LOCAL_CONFIG_ENV "HOME"
@@ -419,9 +420,12 @@ config_read(config_info_t *cfg, const char *appname_in, process_id_t pid, const 
      * if app-specific exists, default at that level is also read to fill in any
      * unspecified values.
      * env vars are always read and used to fill in any unspecified values.
+     * Custom takes precedence over default local.
      * if local exists, global is NOT read.
      */
-    local = my_getenv(L_IF_WIN(LOCAL_CONFIG_ENV), buf, BUFFER_SIZE_BYTES(buf));
+    local = my_getenv(L_IF_WIN(DYNAMORIO_VAR_CONFIGDIR), buf, BUFFER_SIZE_BYTES(buf));
+    if (local == NULL)
+        local = my_getenv(L_IF_WIN(LOCAL_CONFIG_ENV), buf, BUFFER_SIZE_BYTES(buf));
     if (local != NULL) {
         process_id_t pid_to_check = pid;
         if (pid == 0 && cfg == &config)

core/lib/dr_config.h
@@ -943,6 +943,43 @@ dr_nudge_all(client_id_t client_id,
 
 #endif /* WINDOWS */
 
+DR_EXPORT
+/**
+ * Returns in \p config_dir the configuration directory used to store config
+ * files.  In order to use local config files when the normal interactive user
+ * home directory environment variable (HOME on Linux; USERPROFILE on Windows)
+ * is not set and when using one-step configure-and-run, call this routine prior
+ * to creating the child process and pass true for \p alternative_local.  For
+ * multi-step, the caller must set the DYNAMORIO_CONFIGDIR environment variable.
+ *
+ * \param[in]   global          Whether to use global or user-local config
+ *                              files.  On Windows, global config files are
+ *                              stored in a dir pointed at by the DYNAMORIO_HOME
+ *                              registry key.  On Linux, they are in
+ *                              /etc/dynamorio.  Administrative privileges may
+ *                              be needed if global is true.  Note that
+ *                              DynamoRIO gives local config files precedence
+ *                              when both exist.  The caller must separately
+ *                              create the global directory.
+ *
+ * \param[in]   alternative_local Whether to locate a temporary directory to
+ *                                use for user-local config files and to set
+ *                                the DYNAMORIO_CONFIGDIR environment variable
+ *                                to point at it, if the regular local
+ *                                config dir is not found.
+ *
+ * \param[out]  config_dir      Receives the full path to the config dir.
+ *
+ * \param[in]   config_dir_sz   The capacity, in characters, of \p config_dir.
+ *
+ * \return      A dr_config_status_t code indicating the result of the request.
+ */
+dr_config_status_t
+dr_get_config_dir(bool global,
+                  bool alternative_local,
+                  char *config_dir /* OUT */,
+                  size_t config_dir_sz);
+
 /* DR_API EXPORT END */
 
 #endif /* _DR_CONFIG_H_ */

core/lib/globals_shared.h
@@ -852,6 +852,7 @@ typedef char liststring_t[MAX_LIST_OPTION_LENGTH];
 #endif
 
 /* FIXME: the environment vars need to be renamed - it will be a pain */
+#define DYNAMORIO_VAR_CONFIGDIR_ID  DYNAMORIO_CONFIGDIR
 #define DYNAMORIO_VAR_HOME_ID       DYNAMORIO_HOME
 #define DYNAMORIO_VAR_LOGDIR_ID     DYNAMORIO_LOGDIR
 #define DYNAMORIO_VAR_OPTIONS_ID    DYNAMORIO_OPTIONS
@@ -896,6 +897,7 @@ typedef char liststring_t[MAX_LIST_OPTION_LENGTH];
 # define DYNAMORIO_VAR_ANON_PROCESS_BLACKLIST_ID  DYNAMORIO_ANON_PROCESS_BLACKLIST
 #endif
 
+#define DYNAMORIO_VAR_CONFIGDIR  STRINGIFY(DYNAMORIO_VAR_CONFIGDIR_ID)
 #define DYNAMORIO_VAR_HOME       STRINGIFY(DYNAMORIO_VAR_HOME_ID)
 #define DYNAMORIO_VAR_LOGDIR     STRINGIFY(DYNAMORIO_VAR_LOGDIR_ID)
 #define DYNAMORIO_VAR_OPTIONS    STRINGIFY(DYNAMORIO_VAR_OPTIONS_ID)
@@ -941,6 +943,7 @@ typedef char liststring_t[MAX_LIST_OPTION_LENGTH];
 #  define L_IF_WIN(x) L_EXPAND_LEVEL(x)
 
 /* unicode versions of shared names*/
+#  define L_DYNAMORIO_VAR_CONFIGDIR   L_EXPAND_LEVEL(DYNAMORIO_VAR_CONFIGDIR)
 #  define L_DYNAMORIO_VAR_HOME        L_EXPAND_LEVEL(DYNAMORIO_VAR_HOME)
 #  define L_DYNAMORIO_VAR_LOGDIR      L_EXPAND_LEVEL(DYNAMORIO_VAR_LOGDIR)
 #  define L_DYNAMORIO_VAR_OPTIONS     L_EXPAND_LEVEL(DYNAMORIO_VAR_OPTIONS)

core/win32/syscall.c
@@ -1058,6 +1058,29 @@ presys_CreateThreadEx(dcontext_t *dcontext, reg_t *param_base)
 }
 #endif
 
+/***************************************************************************
+ * ENV VAR PROPAGATION
+ */
+
+/* There is some overlap w/ handle_execve() in linux/os.c but not
+ * quite enough to easily share this.
+ */
+static const char * const env_to_propagate[] = {
+    DYNAMORIO_VAR_RUNUNDER,
+    DYNAMORIO_VAR_OPTIONS,
+    DYNAMORIO_VAR_AUTOINJECT,
+    DYNAMORIO_VAR_LOGDIR,
+    DYNAMORIO_VAR_CONFIGDIR,
+};
+static const wchar_t * const wenv_to_propagate[] = {
+    L_DYNAMORIO_VAR_RUNUNDER,
+    L_DYNAMORIO_VAR_OPTIONS,
+    L_DYNAMORIO_VAR_AUTOINJECT,
+    L_DYNAMORIO_VAR_LOGDIR,
+    L_DYNAMORIO_VAR_CONFIGDIR,
+};
+#define NUM_ENV_TO_PROPAGATE (sizeof(env_to_propagate)/sizeof(env_to_propagate[0]))
+
 /* appends DR env vars in the target process PEB */
 static bool
 add_dr_env_vars(dcontext_t *dcontext, HANDLE phandle, wchar_t **env_ptr)
@@ -1067,11 +1090,18 @@ add_dr_env_vars(dcontext_t *dcontext, HANDLE phandle, wchar_t **env_ptr)
     size_t got;
     wchar_t *new_env = NULL;
     wchar_t buf[MAX_OPTIONS_STRING];
-    bool need_rununder = true, need_autoinject = true, need_options = true,
-        need_logdir = true;
-    size_t sz_rununder = 0, sz_autoinject = 0, sz_options = 0, sz_logdir = 0;
+    bool need_var[NUM_ENV_TO_PROPAGATE];
+    size_t sz_var[NUM_ENV_TO_PROPAGATE];
     NTSTATUS res;
     uint old_prot = PAGE_NOACCESS;
+    int i;
+
+    for (i = 0; i < NUM_ENV_TO_PROPAGATE; i++) {
+        if (get_config_val(env_to_propagate[i]) == NULL)
+            need_var[i] = false;
+        else
+            need_var[i] = true;
+    }
 
     ASSERT(env_ptr != NULL);
     if (!nt_read_virtual_memory(phandle, env_ptr, &env, sizeof(env), NULL))
@@ -1099,21 +1129,21 @@ add_dr_env_vars(dcontext_t *dcontext, HANDLE phandle, wchar_t **env_ptr)
             if (buf[0] == 0)
                 break;
             tot_sz += wcslen(buf) + 1;
-            /* if already set we assume has right value */
-            if (wcscmp(L_DYNAMORIO_VAR_RUNUNDER, buf) == 0)
-                need_rununder = false;
-            if (wcscmp(L_DYNAMORIO_VAR_AUTOINJECT, buf) == 0)
-                need_autoinject = false;
-            if (wcscmp(L_DYNAMORIO_VAR_OPTIONS, buf) == 0)
-                need_options = false;
-            if (wcscmp(L_DYNAMORIO_VAR_LOGDIR, buf) == 0)
-                need_logdir = false;
+            for (i = 0; i < NUM_ENV_TO_PROPAGATE; i++) {
+                /* if already set we assume has right value */
+                if (wcscmp(wenv_to_propagate[i], buf) == 0)
+                    need_var[i] = false;
+            }
         }
         tot_sz++; /* final 0 marking end */
         /* from here on out, all *sz vars are total bytes, not wchar_t elements */
         tot_sz *= sizeof(*env);
     }
-    if (!need_rununder && !need_autoinject && !need_options && !need_logdir) {
+    for (i = 0; i < NUM_ENV_TO_PROPAGATE; i++) {
+        if (need_var[i])
+            break;
+    }
+    if (i == NUM_ENV_TO_PROPAGATE) {
         LOG(THREAD, LOG_SYSCALLS, 2,
             "%s: app env vars already contain DR env vars\n", __FUNCTION__);
         return true; /* nothing to do */
@@ -1126,47 +1156,18 @@ add_dr_env_vars(dcontext_t *dcontext, HANDLE phandle, wchar_t **env_ptr)
     /* calculate size needed for adding DR env vars.
      * for each var, we truncate if too big for buf.
      */
-    if (need_rununder) {
-        sz_rununder = wcslen(L_DYNAMORIO_VAR_RUNUNDER) +
-            strlen(get_config_val(DYNAMORIO_VAR_RUNUNDER)) + 2/*=+0*/;
-        if (sz_rununder > BUFFER_SIZE_ELEMENTS(buf)) {
-            SYSLOG_INTERNAL(SYSLOG_WARNING, "truncating DR env var for child");
-            sz_rununder = BUFFER_SIZE_ELEMENTS(buf);
+    for (i = 0; i < NUM_ENV_TO_PROPAGATE; i++) {
+        if (need_var[i]) {
+            sz_var[i] = wcslen(wenv_to_propagate[i]) +
+                strlen(get_config_val(env_to_propagate[i])) + 2/*=+0*/;
+            if (sz_var[i] > BUFFER_SIZE_ELEMENTS(buf)) {
+                SYSLOG_INTERNAL(SYSLOG_WARNING, "truncating DR env var for child");
+                sz_var[i] = BUFFER_SIZE_ELEMENTS(buf);
+            }
+            sz_var[i] *= sizeof(*env);
+            tot_sz += sz_var[i];
         }
-        sz_rununder *= sizeof(*env);
-        tot_sz += sz_rununder;
     }
-    if (need_autoinject) {
-        sz_autoinject = wcslen(L_DYNAMORIO_VAR_AUTOINJECT) +
-            strlen(get_config_val(DYNAMORIO_VAR_AUTOINJECT)) + 2/*=+0*/;
-        if (sz_autoinject > BUFFER_SIZE_ELEMENTS(buf)) {
-            SYSLOG_INTERNAL(SYSLOG_WARNING, "truncating DR env var for child");
-            sz_autoinject = BUFFER_SIZE_ELEMENTS(buf);
-        }
-        sz_autoinject *= sizeof(*env);
-        tot_sz += sz_autoinject;
-    }
-    if (need_options) {
-        sz_options = wcslen(L_DYNAMORIO_VAR_OPTIONS) +
-            strlen(get_config_val(DYNAMORIO_VAR_OPTIONS)) + 2/*=+0*/;
-        if (sz_options > BUFFER_SIZE_ELEMENTS(buf)) {
-            SYSLOG_INTERNAL(SYSLOG_WARNING, "truncating DR env var for child");
-            sz_options = BUFFER_SIZE_ELEMENTS(buf);
-        }
-        sz_options *= sizeof(*env);
-        tot_sz += sz_options;
-    }
-    if (need_logdir) {
-        sz_logdir = wcslen(L_DYNAMORIO_VAR_LOGDIR) +
-            strlen(get_config_val(DYNAMORIO_VAR_LOGDIR)) + 2/*=+0*/;
-        if (sz_logdir > BUFFER_SIZE_ELEMENTS(buf)) {
-            SYSLOG_INTERNAL(SYSLOG_WARNING, "truncating DR env var for child");
-            sz_logdir = BUFFER_SIZE_ELEMENTS(buf);
-        }
-        sz_logdir *= sizeof(*env);
-        tot_sz += sz_logdir;
-    }
-
     /* allocate a new env block and copy over the old */
     res = nt_remote_allocate_virtual_memory(phandle, &new_env, tot_sz,
                                             PAGE_READWRITE, MEM_COMMIT);
@@ -1200,41 +1201,16 @@ add_dr_env_vars(dcontext_t *dcontext, HANDLE phandle, wchar_t **env_ptr)
      * XXX: is alphabetical sorting relied upon?  adding to end is working.
      */
     sz = app_sz - sizeof(*env); /* before final 0 */
-    if (need_rununder) {
-        _snwprintf(buf, BUFFER_SIZE_ELEMENTS(buf), L"%s=%S",
-                   L_DYNAMORIO_VAR_RUNUNDER, get_config_val(DYNAMORIO_VAR_RUNUNDER));
-        NULL_TERMINATE_BUFFER(buf);
-        if (!nt_write_virtual_memory(phandle, new_env + sz/sizeof(*env),
-                                     buf, sz_rununder, &got))
-            goto add_dr_env_failure;
-        sz += sz_rununder;
-    }
-    if (need_autoinject) {
-        _snwprintf(buf, BUFFER_SIZE_ELEMENTS(buf), L"%s=%S",
-                   L_DYNAMORIO_VAR_AUTOINJECT, get_config_val(DYNAMORIO_VAR_AUTOINJECT));
-        NULL_TERMINATE_BUFFER(buf);
-        if (!nt_write_virtual_memory(phandle, new_env + sz/sizeof(*env),
-                                     buf, sz_autoinject, &got))
-            goto add_dr_env_failure;
-        sz += sz_autoinject;
-    }
-    if (need_options) {
-        _snwprintf(buf, BUFFER_SIZE_ELEMENTS(buf), L"%s=%S",
-                   L_DYNAMORIO_VAR_OPTIONS, get_config_val(DYNAMORIO_VAR_OPTIONS));
-        NULL_TERMINATE_BUFFER(buf);
-        if (!nt_write_virtual_memory(phandle, new_env + sz/sizeof(*env),
-                                     buf, sz_options, &got))
-            goto add_dr_env_failure;
-        sz += sz_options;
-    }
-    if (need_logdir) {
-        _snwprintf(buf, BUFFER_SIZE_ELEMENTS(buf), L"%s=%S",
-                   L_DYNAMORIO_VAR_LOGDIR, get_config_val(DYNAMORIO_VAR_LOGDIR));
-        NULL_TERMINATE_BUFFER(buf);
-        if (!nt_write_virtual_memory(phandle, new_env + sz/sizeof(*env),
-                                     buf, sz_logdir, &got))
-            goto add_dr_env_failure;
-        sz += sz_logdir;
+    for (i = 0; i < NUM_ENV_TO_PROPAGATE; i++) {
+        if (need_var[i]) {
+            _snwprintf(buf, BUFFER_SIZE_ELEMENTS(buf), L"%s=%S",
+                       wenv_to_propagate[i], get_config_val(env_to_propagate[i]));
+            NULL_TERMINATE_BUFFER(buf);
+            if (!nt_write_virtual_memory(phandle, new_env + sz/sizeof(*env),
+                                         buf, sz_var[i], &got))
+                goto add_dr_env_failure;
+            sz += sz_var[i];
+        }
     }
     /* write final 0 */
     buf[0] = 0;

libutil/dr_config.c
@@ -309,6 +309,7 @@ free_opt_info(opt_info_t *opt_info)
 #endif
 
 #ifndef PARAMS_IN_REGISTRY
+/* DYNAMORIO_VAR_CONFIGDIR is searched first, and then these: */
 # ifdef WINDOWS
 #  define LOCAL_CONFIG_ENV "USERPROFILE"
 #  define LOCAL_CONFIG_SUBDIR "dynamorio"
@@ -340,13 +341,30 @@ get_config_sfx(dr_platform_t dr_platform)
 }
 
 static bool
-get_config_dir(bool global, char *fname, size_t fname_len)
+env_var_exists(const char *name, char *buf, size_t buflen)
+{
+#ifdef WINDOWS
+    size_t len = GetEnvironmentVariableA(name, buf, (DWORD) buflen);
+    if (len == 0 || len > buflen)
+        return false;
+#else
+    char *val = getenv(LOCAL_CONFIG_ENV);
+    strncpy(buf, val, buflen);
+    buf[buflen - 1] = '\0';
+#endif
+    return true;
+}
+
+/* If find_temp, will use a temp dir; else will fail if no standard config dir. */
+static bool
+get_config_dir(bool global, char *fname, size_t fname_len, bool find_temp)
 {
     char dir[MAXIMUM_PATH];
     const char *subdir;
     if (global) {
 #ifdef WINDOWS
         _snprintf(dir, BUFFER_SIZE_ELEMENTS(dir), TSTR_FMT, get_dynamorio_home());
+        NULL_TERMINATE_BUFFER(dir);
         subdir = GLOBAL_CONFIG_SUBDIR;
 #else
         /* FIXME i#840: Support global config files by porting more of utils.c.
@@ -354,16 +372,33 @@ get_config_dir(bool global, char *fname, size_t fname_len)
         return false;
 #endif
     } else {
+        /* DYNAMORIO_CONFIGDIR takes precedence */
+        if (!env_var_exists(DYNAMORIO_VAR_CONFIGDIR, dir, BUFFER_SIZE_ELEMENTS(dir))) {
+            if (!env_var_exists(LOCAL_CONFIG_ENV, dir, BUFFER_SIZE_ELEMENTS(dir))) {
+                if (!find_temp)
+                    return false;
+                /* Attempt to make things work for non-interactive users (i#939) */
+                if (!env_var_exists("TMP", dir, BUFFER_SIZE_ELEMENTS(dir)) &&
+                    !env_var_exists("TEMP", dir, BUFFER_SIZE_ELEMENTS(dir)) &&
+                    !env_var_exists("TMPDIR", dir, BUFFER_SIZE_ELEMENTS(dir)) &&
+                    !env_var_exists("PWD", dir, BUFFER_SIZE_ELEMENTS(dir))) {
 #ifdef WINDOWS
-        int len = GetEnvironmentVariableA(LOCAL_CONFIG_ENV, dir,
-                                          BUFFER_SIZE_ELEMENTS(dir));
-        if (len <= 0)
-            return false;
+                    return false;
 #else
-        char *home = getenv(LOCAL_CONFIG_ENV);
-        strncpy(dir, home, BUFFER_SIZE_ELEMENTS(dir));
-        NULL_TERMINATE_BUFFER(dir);
+                    strncpy(dir, "/tmp", BUFFER_SIZE_ELEMENTS(dir));
+                    NULL_TERMINATE_BUFFER(dir);
 #endif
+                }
+                /* Ensure DR finds the same config file! */
+#ifdef WINDOWS
+                if (!SetEnvironmentVariableA(DYNAMORIO_VAR_CONFIGDIR, dir))
+                    return false;
+#else
+                if (setenv(DYNAMORIO_VAR_CONFIGDIR, dir, 0) != 0)
+                    return false;
+#endif
+            }
+        }
         subdir = LOCAL_CONFIG_SUBDIR;
     }
     _snprintf(fname, fname_len, "%s/%s", dir, subdir);
@@ -385,7 +420,7 @@ get_config_file_name(const char *process_name,
                      size_t fname_len)
 {
     size_t dir_len;
-    if (!get_config_dir(global, fname, fname_len)) {
+    if (!get_config_dir(global, fname, fname_len, false)) {
         DO_ASSERT(false && "get_config_dir failed");
         return false;
     }
@@ -1267,7 +1302,7 @@ dr_registered_process_iterator_start(dr_platform_t dr_platform,
     else
         iter->cur = NULL;
 #else
-    if (!get_config_dir(global, iter->dir, BUFFER_SIZE_ELEMENTS(iter->dir))) {
+    if (!get_config_dir(global, iter->dir, BUFFER_SIZE_ELEMENTS(iter->dir), false)) {
         iter->has_next = false;
         return iter;
     }
@@ -1829,3 +1864,24 @@ dr_nudge_all(client_id_t client_id, uint64 arg, uint timeout_ms, int *nudge_coun
 }
 
 #endif /* WINDOWS */
+
+/* XXX: perhaps we should take in a config dir as a parameter to all
+ * of the registration routines in the drconfiglib API rather than or
+ * in addition to having this env var DYNAMORIO_CONFIGLIB.
+ * Xref i#939.
+ */
+dr_config_status_t
+dr_get_config_dir(bool global,
+                  bool alternative_local,
+                  char *config_dir /* OUT */,
+                  size_t config_dir_sz)
+{
+    if (get_config_dir(global, config_dir, config_dir_sz, alternative_local)) {
+        /* XXX: it would be nice to return DR_CONFIG_STRING_TOO_LONG if the
+         * buffer is too small, rather than just truncating it
+         */
+        return DR_SUCCESS;
+    } else
+        return DR_FAILURE;
+}
+

commit 6d6be6388c7872f8e3164cc7e7e9f4b42c8e4feb

i#1073: Initialize prop_idx elements to -1 in handle_execve()
This fixes a UMR regression introduced in r1661.

Fixes issue 1073.

Review URL: https://codereview.appspot.com/7303073

SVN-Revision: 1889

Match-id-79507defc59bff17de238d1988c3b36aef6aaf56

core/linux/os.c
@@ -4834,8 +4834,10 @@ handle_execve(dcontext_t *dcontext)
     inject_library_path = IF_X64_ELSE(x64, !x64) ? dynamorio_library_path :
         dynamorio_alt_arch_path;
 
-    for (j = 0; j < NUM_ENV_TO_PROPAGATE; j++)
+    for (j = 0; j < NUM_ENV_TO_PROPAGATE; j++) {
         prop_found[j] = -1;
+        prop_idx[j] = -1;
+    }
 
     if (envp == NULL) {
         LOG(THREAD, LOG_SYSCALLS, 3, "\tenv is NULL\n");