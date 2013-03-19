ptrbman/dynamorio

commit eb1aa7f88e4561d9410a6a9d9eb89cee3706ba7f

i#1101: Reorder lock ranks for coarse info table after incoming
Fixes issue 1101.

Review URL: https://codereview.appspot.com/7457044

git-svn-id: https://dynamorio.googlecode.com/svn/trunk@1965 49cc7528-f6fd-11dd-9d1a-b59b2e1864b6

core/utils.h
@@ -382,6 +382,18 @@ enum {
     LOCK_RANK(coarse_info_lock), /* < special_heap_lock, < global_alloc_lock,
                                   * > change_linking_lock */
 
+    LOCK_RANK(executable_areas), /* < dynamo_areas < global_alloc_lock
+                                  * < process_module_vector_lock (diagnostics)
+                                  */
+    LOCK_RANK(module_data_lock),  /* < loaded_module_areas, < special_heap_lock,
+                                   * > executable_areas */
+    LOCK_RANK(special_units_list_lock), /* < special_heap_lock */
+    LOCK_RANK(special_heap_lock), /* > bb_building_lock, > hotp_vul_table_lock
+                                   * < dynamo_areas, < heap_unit_lock */
+    LOCK_RANK(coarse_info_incoming_lock), /* < coarse_table_rwlock
+                                           * > special_heap_lock, > coarse_info_lock,
+                                           * > change_linking_lock */
+
     /* (We don't technically need a coarse_table_rwlock separate from table_rwlock
      * anymore but having it gives us flexibility so I'm leaving it)
      */
@@ -392,16 +404,6 @@ enum {
     /* We make the th table separate (we look in it while holding master table lock) */
     LOCK_RANK(coarse_th_table_rwlock), /* < global_alloc_lock */
 
-    LOCK_RANK(executable_areas), /* < dynamo_areas < global_alloc_lock
-                                  * < process_module_vector_lock (diagnostics)
-                                  */
-    LOCK_RANK(module_data_lock),  /* < loaded_module_areas, < special_heap_lock,
-                                   * > executable_areas */
-    LOCK_RANK(special_units_list_lock), /* < special_heap_lock */
-    LOCK_RANK(special_heap_lock), /* > bb_building_lock, > hotp_vul_table_lock
-                                   * < dynamo_areas, < heap_unit_lock */
-    LOCK_RANK(coarse_info_incoming_lock), /* > special_heap_lock, > coarse_info_lock,
-                                           * > change_linking_lock */
 #ifdef RCT_IND_BRANCH
     LOCK_RANK(rct_module_lock), /* > coarse_info_lock, > executable_areas,
                                  * < heap allocation */

commit 60b5c15f413151fc05a6ed6573a408d3eefebe8a

Fix lock RCT/RAT rank order regression from r1965
SVN-Revision: 2006

Match-id-ec57abe2f8401b1c278107582986845ef87f9514

core/utils.h
@@ -386,6 +386,15 @@ enum {
     LOCK_RANK(executable_areas), /* < dynamo_areas < global_alloc_lock
                                   * < process_module_vector_lock (diagnostics)
                                   */
+#ifdef RCT_IND_BRANCH
+    LOCK_RANK(rct_module_lock), /* > coarse_info_lock, > executable_areas,
+                                 * < module_data_lock, < heap allocation */
+#endif
+#ifdef RETURN_AFTER_CALL
+    LOCK_RANK(after_call_lock), /* < table_rwlock, > bb_building_lock,
+                                 * > coarse_info_lock, > executable_areas,
+                                 * < module_data_lock */
+#endif
     LOCK_RANK(module_data_lock),  /* < loaded_module_areas, < special_heap_lock,
                                    * > executable_areas */
     LOCK_RANK(special_units_list_lock), /* < special_heap_lock */
@@ -405,14 +414,6 @@ enum {
     /* We make the th table separate (we look in it while holding master table lock) */
     LOCK_RANK(coarse_th_table_rwlock), /* < global_alloc_lock */
 
-#ifdef RCT_IND_BRANCH
-    LOCK_RANK(rct_module_lock), /* > coarse_info_lock, > executable_areas,
-                                 * < heap allocation */
-#endif
-#ifdef RETURN_AFTER_CALL
-    LOCK_RANK(after_call_lock), /* < table_rwlock, > bb_building_lock,
-                                 * > coarse_info_lock, > executable_areas */
-#endif
     LOCK_RANK(process_module_vector_lock), /* < snapshot_lock > all_threads_synch_lock */
     /* For Loglevel 1 and higher, with LOG_MEMSTATS, the snapshot lock is
      * grabbed on an exception, possible deadlock if already held FIXME */