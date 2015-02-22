ptrbman/dynamorio

commit 51dc400193d84903f77818db7ff422cf9bfd9403

i#1551 port to ARM: add safe_read fault handling
Adds ARM safe_read fault handling:
+ Implements asm routine master_signal_handler()
+ Adjusts master_signal_handler_C() for ARM
+ Implements is_safe_read_pc() for ARM
+ Implements dump_sigcontext()

Review-URL: https://codereview.appspot.com/203250043

core/arch/arm/arm.asm
@@ -40,6 +40,9 @@ START_FILE
 #ifndef NOT_DYNAMORIO_CORE_PROPER
 DECL_EXTERN(dynamorio_app_take_over_helper)
 #endif /* !NOT_DYNAMORIO_CORE_PROPER */
+#if defined(UNIX)
+DECL_EXTERN(master_signal_handler_C)
+#endif
 
 DECL_EXTERN(exiting_thread_count)
 DECL_EXTERN(initstack)
@@ -381,6 +384,9 @@ GLOBAL_LABEL(global_do_syscall_int:)
         END_FUNC(global_do_syscall_int)
 
 
+DECLARE_GLOBAL(safe_read_asm_pre)
+DECLARE_GLOBAL(safe_read_asm_mid)
+DECLARE_GLOBAL(safe_read_asm_post)
 DECLARE_GLOBAL(safe_read_asm_recover)
 
 /* i#350: We implement safe_read in assembly and save the PCs that can fault.
@@ -400,7 +406,10 @@ DECLARE_GLOBAL(safe_read_asm_recover)
 GLOBAL_LABEL(safe_read_asm:)
         cmp      ARG3,   #0
 1:      beq      safe_read_asm_recover
+ADDRTAKEN_LABEL(safe_read_asm_pre:)
         ldrb     REG_R3, [ARG2]
+ADDRTAKEN_LABEL(safe_read_asm_mid:)
+ADDRTAKEN_LABEL(safe_read_asm_post:)
         strb     REG_R3, [ARG1]
         subs     ARG3, ARG3, #1
         add      ARG2, ARG2, #1
@@ -551,9 +560,15 @@ GLOBAL_LABEL(dynamorio_nonrt_sigreturn:)
         bl       GLOBAL_REF(unexpected_return)
         END_FUNC(dynamorio_nonrt_sigreturn)
 
+
+#ifndef HAVE_SIGALTSTACK
+# error NYI
+#endif
         DECLARE_FUNC(master_signal_handler)
 GLOBAL_LABEL(master_signal_handler:)
-        /* FIXME i#1551: NYI on ARM */
+        mov      ARG4, sp /* pass as extra arg */
+        b        GLOBAL_REF(master_signal_handler_C)
+        /* master_signal_handler_C will do the ret */
         bl       GLOBAL_REF(unexpected_return)
         END_FUNC(master_signal_handler)
 

core/arch/x86_code.c
@@ -424,14 +424,9 @@ safe_read_fast(const void *base, size_t size, void *out_buf, size_t *bytes_read)
 bool
 is_safe_read_pc(app_pc pc)
 {
-#ifdef X86
     return (pc == (app_pc)safe_read_asm_pre ||
             pc == (app_pc)safe_read_asm_mid ||
             pc == (app_pc)safe_read_asm_post);
-#elif defined(ARM)
-    /* FIXME i#1551: NYI */
-    return false;
-#endif
 }
 
 app_pc

core/unix/os_exports.h
@@ -1,5 +1,5 @@
 /* **********************************************************
- * Copyright (c) 2011-2014 Google, Inc.  All rights reserved.
+ * Copyright (c) 2011-2015 Google, Inc.  All rights reserved.
  * Copyright (c) 2000-2010 VMware, Inc.  All rights reserved.
  * **********************************************************/
 
@@ -364,13 +364,26 @@ typedef struct sigcontext sigcontext_t;
 # define SC_SYSNUM_REG SC_XAX
 #elif defined(ARM)
 # ifdef X64
+   /* FIXME i#1569: NYI */
 #  error 64-bit ARM is not supported
 # else
 #  define SC_XIP SC_FIELD(arm_pc)
-#  define SC_XSP SC_FIELD(arm_sp)
 #  define SC_FP  SC_FIELD(arm_fp)
 #  define SC_R0  SC_FIELD(arm_r0)
+#  define SC_R1  SC_FIELD(arm_r1)
+#  define SC_R2  SC_FIELD(arm_r2)
+#  define SC_R3  SC_FIELD(arm_r3)
+#  define SC_R4  SC_FIELD(arm_r4)
+#  define SC_R5  SC_FIELD(arm_r5)
+#  define SC_R6  SC_FIELD(arm_r6)
 #  define SC_R7  SC_FIELD(arm_r7)
+#  define SC_R8  SC_FIELD(arm_r8)
+#  define SC_R9  SC_FIELD(arm_r9)
+#  define SC_R10 SC_FIELD(arm_r10)
+#  define SC_R11 SC_FIELD(arm_fp)
+#  define SC_R12 SC_FIELD(arm_ip)
+#  define SC_XSP SC_FIELD(arm_sp)
+#  define SC_LR  SC_FIELD(arm_lr)
 #  define SC_XFLAGS SC_FIELD(arm_cpsr)
 #  define SC_SYSNUM_REG SC_R7
 # endif /* 64/32-bit */

core/unix/signal.c
@@ -3628,7 +3628,7 @@ is_safe_read_ucxt(kernel_ucontext_t *ucxt)
  * WARNING: behavior varies with different versions of the kernel!
  * sigaction support was only added with 2.2
  */
-#ifdef X64
+#ifndef X86_32
 /* stub in x86.asm passes our xsp to us */
 # ifdef MACOS
 void
@@ -3648,7 +3648,7 @@ master_signal_handler_C(byte *xsp)
 #endif
 {
     sigframe_rt_t *frame = (sigframe_rt_t *) xsp;
-#ifndef X64
+#ifdef X86_32
     /* Read the normal arguments from the frame. */
     int sig = frame->sig;
     siginfo_t *siginfo = frame->pinfo;
@@ -3762,7 +3762,7 @@ master_signal_handler_C(byte *xsp)
         siginfo->si_code);
     DOLOG(level+1, LOG_ASYNCH, { dump_sigcontext(dcontext, sc); });
 
-#if !defined(X64) && !defined(VMX86_SERVER) && defined(LINUX)
+#if defined(X86_32) && !defined(VMX86_SERVER) && defined(LINUX)
     /* FIXME case 6700: 2.6.9 (FC3) kernel sets up our frame with a pretcode
      * of 0x440.  This happens if our restorer is unspecified (though 2.6.9
      * src code shows setting the restorer to a default value in that case...)

core/unix/signal_linux_arm.c
@@ -1,5 +1,5 @@
 /* **********************************************************
- * Copyright (c) 2014 Google, Inc.  All rights reserved.
+ * Copyright (c) 2014-2015 Google, Inc.  All rights reserved.
  * **********************************************************/
 
 /*
@@ -60,8 +60,27 @@ save_fpstate(dcontext_t *dcontext, sigframe_rt_t *frame)
 void
 dump_sigcontext(dcontext_t *dcontext, sigcontext_t *sc)
 {
-    /* FIXME i#1551: NYI on ARM */
-    ASSERT_NOT_IMPLEMENTED(false);
+#ifdef X64
+  /* FIXME i#1569: NYI */
+# error NYI
+#endif
+    LOG(THREAD, LOG_ASYNCH, 1, "\tr0  ="PFX"\n", sc->SC_R0);
+    LOG(THREAD, LOG_ASYNCH, 1, "\tr1  ="PFX"\n", sc->SC_R1);
+    LOG(THREAD, LOG_ASYNCH, 1, "\tr2  ="PFX"\n", sc->SC_R2);
+    LOG(THREAD, LOG_ASYNCH, 1, "\tr3  ="PFX"\n", sc->SC_R3);
+    LOG(THREAD, LOG_ASYNCH, 1, "\tr4  ="PFX"\n", sc->SC_R4);
+    LOG(THREAD, LOG_ASYNCH, 1, "\tr5  ="PFX"\n", sc->SC_R5);
+    LOG(THREAD, LOG_ASYNCH, 1, "\tr6  ="PFX"\n", sc->SC_R6);
+    LOG(THREAD, LOG_ASYNCH, 1, "\tr7  ="PFX"\n", sc->SC_R7);
+    LOG(THREAD, LOG_ASYNCH, 1, "\tr8  ="PFX"\n", sc->SC_R8);
+    LOG(THREAD, LOG_ASYNCH, 1, "\tr9  ="PFX"\n", sc->SC_R9);
+    LOG(THREAD, LOG_ASYNCH, 1, "\tr10 ="PFX"\n", sc->SC_R10);
+    LOG(THREAD, LOG_ASYNCH, 1, "\tr11 ="PFX"\n", sc->SC_R11);
+    LOG(THREAD, LOG_ASYNCH, 1, "\tr12 ="PFX"\n", sc->SC_R12);
+    LOG(THREAD, LOG_ASYNCH, 1, "\tsp  ="PFX"\n", sc->SC_XSP);
+    LOG(THREAD, LOG_ASYNCH, 1, "\tr14 ="PFX"\n", sc->SC_LR);
+    LOG(THREAD, LOG_ASYNCH, 1, "\tpc  ="PFX"\n", sc->SC_XIP);
+    LOG(THREAD, LOG_ASYNCH, 1, "\tcpsr="PFX"\n", sc->SC_XFLAGS);
 }
 #endif /* DEBUG */
 

commit 05f1c30f1f22c3e87d86fdb3cc3a3168f16c58ca

i#1551 port to ARM: fix drdecodelib build
Add NOT_DYNAMORIO_CORE_PROPER around master_signal_handler to fix drdecode
build broken by 51dc400193d84903f77818db7ff422cf9bfd9403.

Review-URL: https://codereview.appspot.com/198650043

Match-id-7870ecf251f0efd453825a7c672fa571d52f3724

core/arch/arm/arm.asm
@@ -560,6 +560,7 @@ GLOBAL_LABEL(dynamorio_nonrt_sigreturn:)
         bl       GLOBAL_REF(unexpected_return)
         END_FUNC(dynamorio_nonrt_sigreturn)
 
+#ifndef NOT_DYNAMORIO_CORE_PROPER
 
 #ifndef HAVE_SIGALTSTACK
 # error NYI
@@ -574,6 +575,7 @@ GLOBAL_LABEL(master_signal_handler:)
 
 #endif /* LINUX */
 
+#endif /* NOT_DYNAMORIO_CORE_PROPER */
 
 /* void hashlookup_null_handler(void)
  * PR 305731: if the app targets NULL, it ends up here, which indirects