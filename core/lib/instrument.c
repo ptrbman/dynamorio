ptrbman/dynamorio

commit 3db7e9060b3e357512300ecdc92fd70eb29b1f4a

i#556: add support to detect a console handle in win8+
Review-URL: https://codereview.appspot.com/284970043

core/lib/instrument.c
@@ -4255,6 +4255,26 @@ DR_API
 bool
 dr_using_console(void)
 {
+    FILE_FS_DEVICE_INFORMATION DeviceInformation;
+    HANDLE herr = get_stderr_handle();
+    if (get_os_version() >= WINDOWS_VERSION_8) {
+        /* The handle is invalid iff it's a gui app and the parent is a console */
+        if (herr == INVALID_HANDLE_VALUE) {
+            module_data_t *app_kernel32 = dr_lookup_module_by_name("kernel32.dll");
+            if (privload_attach_parent_console(app_kernel32->start) == false) {
+                dr_free_module_data(app_kernel32);
+                return false;
+            }
+            dr_free_module_data(app_kernel32);
+            herr = get_stderr_handle();
+        }
+        if (nt_query_volume_info(herr, &DeviceInformation, sizeof(DeviceInformation),
+                                 FileFsDeviceInformation) == STATUS_SUCCESS) {
+            if (DeviceInformation.DeviceType == FILE_DEVICE_CONSOLE)
+                return true;
+        }
+        return false;
+    }
     /* We detect cmd window using what kernel32!WriteFile uses: a handle
      * having certain bits set.
      */

core/module_shared.h
@@ -607,6 +607,9 @@ privload_load_finalized(privmod_t *mod);
 #ifdef WINDOWS
 bool
 privload_console_share(app_pc priv_kernel32, app_pc app_kernel32);
+
+bool
+privload_attach_parent_console(app_pc app_kernel32);
 #endif
 
 #endif /* MODULE_LIST_H */

core/win32/loader.c
@@ -1825,6 +1825,30 @@ privload_disable_console_init(privmod_t *mod)
     return success;
 }
 
+/* GUI apps are initialized without a console. To enable writing to the console
+ * we attach to the parent's console.
+ * XXX: if an app attempts to create/attach to a console w/o first freeing itself
+ * from this console, it will fail since a process can only associate w/ one console.
+ * The solution here would be to monitor such attempts by the app and free the console
+ * that is setup here.
+ */
+typedef BOOL (WINAPI *kernel32_AttachConsole_t) (IN DWORD);
+static kernel32_AttachConsole_t kernel32_AttachConsole;
+
+bool
+privload_attach_parent_console(app_pc app_kernel32)
+{
+    ASSERT(app_kernel32 != NULL);
+    if (kernel32_AttachConsole == NULL)
+        kernel32_AttachConsole = (kernel32_AttachConsole_t)
+            get_proc_address(app_kernel32, "AttachConsole");
+    if (kernel32_AttachConsole != NULL) {
+        if (kernel32_AttachConsole(ATTACH_PARENT_PROCESS) != 0)
+            return true;
+    }
+    return false;
+}
+
 /* i#556: A process can be associated with only one console, which is why the call to
  * ConnectConsoleInternal is nop'd out for win7. With the call disabled priv kernel32
  * does not initialize globals needed for console support. This routine will share the
@@ -1849,22 +1873,9 @@ privload_console_share(app_pc priv_kernel32, app_pc app_kernel32)
     static const uint MAX_DECODE = 1024;
 
     ASSERT(app_kernel32 != NULL);
-    /* GUI apps are initialized without a console. To enable writing to the console
-     * we attach to the parent's console.
-     * XXX: if an app attempts to create/attach to a console w/o first freeing itself
-     * from this console, it will fail since a process can only associate w/ one console.
-     * The solution here would be to monitor such attempts by the app and free the console
-     * that is setup here.
-     */
     if (get_own_peb()->ImageSubsystem != IMAGE_SUBSYSTEM_WINDOWS_CUI) {
-        kernel32_AttachConsole = (kernel32_AttachConsole_t)
-            get_proc_address(app_kernel32, "AttachConsole");
-        if (kernel32_AttachConsole == NULL)
+        if (privload_attach_parent_console(app_kernel32) == false)
             return false;
-        status = kernel32_AttachConsole(ATTACH_PARENT_PROCESS);
-        if (status == 0) {
-            return false;
-        }
     }
     /* xref i#440: Noping out the call to ConsoleConnectInternal is enough to get console
      * support for wow64. We have this check after in case of GUI app.

core/win32/ntdll_types.h
@@ -486,4 +486,6 @@ typedef struct _RTL_USER_PROCESS_PARAMETERS {
 #define FILE_PIPE_QUEUE_OPERATION       0x00000000
 #define FILE_PIPE_COMPLETE_OPERATION    0x00000001
 
+#define FILE_DEVICE_CONSOLE 0x00000050
+
 #endif /* _NTDLL_TYPES_H_ */

commit 1616694d42ed3707a085fa3167aaf7c2d0005750

i#556: fix compile error in 3db7e90
Fixes VS2010 build breakage from 3db7e90.

Review-URL: https://codereview.appspot.com/282950043

Match-id-da61ab95bece4c5c6e2791817956d07738657c72

core/lib/instrument.c
@@ -1,5 +1,5 @@
 /* ******************************************************************************
- * Copyright (c) 2010-2015 Google, Inc.  All rights reserved.
+ * Copyright (c) 2010-2016 Google, Inc.  All rights reserved.
  * Copyright (c) 2010-2011 Massachusetts Institute of Technology  All rights reserved.
  * Copyright (c) 2002-2010 VMware, Inc.  All rights reserved.
  * ******************************************************************************/
@@ -4255,9 +4255,10 @@ DR_API
 bool
 dr_using_console(void)
 {
-    FILE_FS_DEVICE_INFORMATION DeviceInformation;
-    HANDLE herr = get_stderr_handle();
+    bool res;
     if (get_os_version() >= WINDOWS_VERSION_8) {
+        FILE_FS_DEVICE_INFORMATION device_info;
+        HANDLE herr = get_stderr_handle();
         /* The handle is invalid iff it's a gui app and the parent is a console */
         if (herr == INVALID_HANDLE_VALUE) {
             module_data_t *app_kernel32 = dr_lookup_module_by_name("kernel32.dll");
@@ -4268,9 +4269,9 @@ dr_using_console(void)
             dr_free_module_data(app_kernel32);
             herr = get_stderr_handle();
         }
-        if (nt_query_volume_info(herr, &DeviceInformation, sizeof(DeviceInformation),
+        if (nt_query_volume_info(herr, &device_info, sizeof(device_info),
                                  FileFsDeviceInformation) == STATUS_SUCCESS) {
-            if (DeviceInformation.DeviceType == FILE_DEVICE_CONSOLE)
+            if (device_info.DeviceType == FILE_DEVICE_CONSOLE)
                 return true;
         }
         return false;
@@ -4278,7 +4279,7 @@ dr_using_console(void)
     /* We detect cmd window using what kernel32!WriteFile uses: a handle
      * having certain bits set.
      */
-    bool res = (((ptr_int_t)get_stderr_handle() & 0x10000003) == 0x3);
+    res = (((ptr_int_t)get_stderr_handle() & 0x10000003) == 0x3);
     CLIENT_ASSERT(!res || get_os_version() < WINDOWS_VERSION_8,
                   "Please report this: Windows 8 does have old-style consoles!");
     return res;