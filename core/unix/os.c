ptrbman/dynamorio

commit c4b55cb12c773f093ddcacb8b425557d28595fc6

i#1569 AArch64: Adapt for syscalls that Linux arm64 does not have.
AArch64 uses the UAPI syscall numbers, in common with some other recent
architectures. Some traditional system calls are missing from this
list so code that refers to them is adapted. A typical example is
open(file, flags, mode) replaced by openat(AT_FDCWD, file, flags, mode).

The syscall list itself is not yet included with this commit.

Review-URL: https://codereview.appspot.com/292880043

core/unix/os.c
@@ -115,29 +115,6 @@ struct compat_rlimit {
 # define F_DUPFD_CLOEXEC 1030
 #endif
 
-/* Cross arch syscall nums for use with struct stat64. */
-#ifdef X64
-# define SYSNUM_STAT SYS_stat
-# define SYSNUM_FSTAT SYS_fstat
-#else
-# define SYSNUM_STAT SYS_stat64
-# define SYSNUM_FSTAT SYS_fstat64
-#endif
-
-#ifdef MACOS
-# define SYSNUM_EXIT_PROCESS SYS_exit
-# define SYSNUM_EXIT_THREAD SYS_bsdthread_terminate
-#else
-# define SYSNUM_EXIT_PROCESS SYS_exit_group
-# define SYSNUM_EXIT_THREAD SYS_exit
-#endif
-
-#ifdef ANDROID
-/* Custom prctl flags specific to Android (xref i#1861) */
-# define PR_SET_VMA   0x53564d41
-# define PR_SET_VMA_ANON_NAME    0
-#endif
-
 /* This is not always sufficient to identify a syscall return value.
  * For example, MacOS has some 32-bit syscalls that return 64-bit
  * values in xdx:xax.
@@ -202,6 +179,31 @@ char **our_environ;
 
 #ifdef CLIENT_INTERFACE
 # include "instrument.h"
+#endif
+
+/* Cross arch syscall nums for use with struct stat64. */
+#ifdef X64
+# ifdef SYS_stat
+#  define SYSNUM_STAT SYS_stat
+# endif
+# define SYSNUM_FSTAT SYS_fstat
+#else
+# define SYSNUM_STAT SYS_stat64
+# define SYSNUM_FSTAT SYS_fstat64
+#endif
+
+#ifdef MACOS
+# define SYSNUM_EXIT_PROCESS SYS_exit
+# define SYSNUM_EXIT_THREAD SYS_bsdthread_terminate
+#else
+# define SYSNUM_EXIT_PROCESS SYS_exit_group
+# define SYSNUM_EXIT_THREAD SYS_exit
+#endif
+
+#ifdef ANDROID
+/* Custom prctl flags specific to Android (xref i#1861) */
+# define PR_SET_VMA   0x53564d41
+# define PR_SET_VMA_ANON_NAME    0
 #endif
 
 #ifdef NOT_DYNAMORIO_CORE_PROPER
@@ -2387,13 +2389,14 @@ set_thread_private_dcontext(dcontext_t *dcontext)
 #endif
 }
 
+#ifdef SYS_fork
 /* replaces old with new
  * use for forking: child should replace parent's id with its own
  */
 static void
 replace_thread_id(thread_id_t old, thread_id_t new)
 {
-#ifdef HAVE_TLS
+# ifdef HAVE_TLS
     thread_id_t new_tid = new;
     ASSERT(is_thread_tls_initialized());
     DOCHECK(1, {
@@ -2402,7 +2405,7 @@ replace_thread_id(thread_id_t old, thread_id_t new)
         ASSERT(old_tid == old);
     });
     WRITE_TLS_INT_SLOT_IMM(TLS_THREAD_ID_OFFSET, new_tid);
-#else
+# else
     int i;
     mutex_lock(&tls_lock);
     for (i=0; i<MAX_THREADS; i++) {
@@ -2412,8 +2415,9 @@ replace_thread_id(thread_id_t old, thread_id_t new)
         }
     }
     mutex_unlock(&tls_lock);
-#endif
+# endif
 }
+#endif
 
 #endif /* !NOT_DYNAMORIO_CORE_PROPER */
 
@@ -3479,12 +3483,22 @@ llseek_syscall(int fd, int64 offset, int origin, int64 *result)
 #endif
 }
 
+static ptr_int_t
+dynamorio_syscall_stat(const char *fname, struct stat64 *st)
+{
+#ifdef SYSNUM_STAT
+    return dynamorio_syscall(SYSNUM_STAT, 2, fname, st);
+#else
+    return dynamorio_syscall(SYS_fstatat, 4, AT_FDCWD, fname, st, 0);
+#endif
+}
+
 bool
 os_file_exists(const char *fname, bool is_dir)
 {
     /* _LARGEFILE64_SOURCE should make libc struct match kernel (see top of file) */
     struct stat64 st;
-    ptr_int_t res = dynamorio_syscall(SYSNUM_STAT, 2, fname, &st);
+    ptr_int_t res = dynamorio_syscall_stat(fname, &st);
     if (res != 0) {
         LOG(THREAD_GET, LOG_SYSCALLS, 2, "%s failed: "PIFX"\n", __func__, res);
         return false;
@@ -3498,12 +3512,12 @@ bool
 os_files_same(const char *path1, const char *path2)
 {
     struct stat64 st1, st2;
-    ptr_int_t res = dynamorio_syscall(SYSNUM_STAT, 2, path1, &st1);
+    ptr_int_t res = dynamorio_syscall_stat(path1, &st1);
     if (res != 0) {
         LOG(THREAD_GET, LOG_SYSCALLS, 2, "%s failed: "PIFX"\n", __func__, res);
         return false;
     }
-    res = dynamorio_syscall(SYSNUM_STAT, 2, path2, &st2);
+    res = dynamorio_syscall_stat(path2, &st2);
     if (res != 0) {
         LOG(THREAD_GET, LOG_SYSCALLS, 2, "%s failed: "PIFX"\n", __func__, res);
         return false;
@@ -3516,7 +3530,7 @@ os_get_file_size(const char *file, uint64 *size)
 {
     /* _LARGEFILE64_SOURCE should make libc struct match kernel (see top of file) */
     struct stat64 st;
-    ptr_int_t res = dynamorio_syscall(SYSNUM_STAT, 2, file, &st);
+    ptr_int_t res = dynamorio_syscall_stat(file, &st);
     if (res != 0) {
         LOG(THREAD_GET, LOG_SYSCALLS, 2, "%s failed: "PIFX"\n", __func__, res);
         return false;
@@ -3548,7 +3562,11 @@ bool
 os_create_dir(const char *fname, create_directory_flags_t create_dir_flags)
 {
     bool require_new = TEST(CREATE_DIR_REQUIRE_NEW, create_dir_flags);
+#ifdef SYS_mkdir
     int rc = dynamorio_syscall(SYS_mkdir, 2, fname, S_IRWXU|S_IRWXG);
+#else
+    int rc = dynamorio_syscall(SYS_mkdirat, 3, AT_FDCWD, fname, S_IRWXU|S_IRWXG);
+#endif
     ASSERT(create_dir_flags == CREATE_DIR_REQUIRE_NEW ||
            create_dir_flags == CREATE_DIR_ALLOW_EXISTING);
     return (rc == 0 || (!require_new && rc == -EEXIST));
@@ -3557,14 +3575,24 @@ os_create_dir(const char *fname, create_directory_flags_t create_dir_flags)
 bool
 os_delete_dir(const char *name)
 {
+#ifdef SYS_rmdir
     return (dynamorio_syscall(SYS_rmdir, 1, name) == 0);
+#else
+    return (dynamorio_syscall(SYS_unlinkat, 3,
+                              AT_FDCWD, name, AT_REMOVEDIR) == 0);
+#endif
 }
 
 int
 open_syscall(const char *file, int flags, int mode)
 {
     ASSERT(file != NULL);
+#ifdef SYS_open
     return dynamorio_syscall(SYSNUM_NO_CANCEL(SYS_open), 3, file, flags, mode);
+#else
+    return dynamorio_syscall(SYSNUM_NO_CANCEL(SYS_openat), 4,
+                             AT_FDCWD, file, flags, mode);
+#endif
 }
 
 int
@@ -3866,7 +3894,11 @@ os_tell(file_t f)
 bool
 os_delete_file(const char *name)
 {
+#ifdef SYS_unlink
     return (dynamorio_syscall(SYS_unlink, 1, name) == 0);
+#else
+    return (dynamorio_syscall(SYS_unlinkat, 2, AT_FDCWD, name) == 0);
+#endif
 }
 
 bool
@@ -3877,7 +3909,7 @@ os_rename_file(const char *orig_name, const char *new_name, bool replace)
         /* SYS_rename replaces so we must test beforehand => could have race */
         /* _LARGEFILE64_SOURCE should make libc struct match kernel (see top of file) */
         struct stat64 st;
-        ptr_int_t res = dynamorio_syscall(SYSNUM_STAT, 2, new_name, &st);
+        ptr_int_t res = dynamorio_syscall_stat(new_name, &st);
         if (res == 0)
             return false;
         else if (res != -ENOENT) {
@@ -3885,7 +3917,12 @@ os_rename_file(const char *orig_name, const char *new_name, bool replace)
             return false;
         }
     }
+#ifdef SYS_rename
     res = dynamorio_syscall(SYS_rename, 2, orig_name, new_name);
+#else
+    res = dynamorio_syscall(SYS_renameat, 4,
+                            AT_FDCWD, orig_name, AT_FDCWD, new_name);
+#endif
     if (res != 0)
         LOG(THREAD_GET, LOG_SYSCALLS, 2, "%s \"%s\" to \"%s\" failed: "PIFX"\n",
             __func__, orig_name, new_name, res);
@@ -4526,8 +4563,12 @@ ignorable_system_call_normalized(int num)
     case SYS_bsdthread_create:
     case SYS_posix_spawn:
 #endif
+#ifdef SYS_fork
     case SYS_fork:
+#endif
+#ifdef SYS_vfork
     case SYS_vfork:
+#endif
     case SYS_kill:
 #if defined(SYS_tkill)
     case SYS_tkill:
@@ -4553,7 +4594,9 @@ ignorable_system_call_normalized(int num)
     case SYS_rt_sigtimedwait:
     case SYS_rt_sigqueueinfo:
     case SYS_rt_sigsuspend:
+#ifdef SYS_signalfd
     case SYS_signalfd:
+#endif
     case SYS_signalfd4:
 #endif
     case SYS_sigaltstack:
@@ -4567,7 +4610,9 @@ ignorable_system_call_normalized(int num)
     case SYS_close_nocancel:
 #endif
     case SYS_close:
+#ifdef SYS_dup2
     case SYS_dup2:
+#endif
 #ifdef LINUX
     case SYS_dup3:
 #endif
@@ -4603,7 +4648,7 @@ ignorable_system_call_normalized(int num)
     case SYS_cacheflush:
 #endif
         return false;
-#ifdef LINUX
+#ifdef SYS_readlink
     case SYS_readlink:
         return !DYNAMO_OPTION(early_inject);
 #endif
@@ -4940,8 +4985,15 @@ is_thread_create_syscall_helper(ptr_uint_t sysnum, ptr_uint_t flags)
      */
     return (sysnum == SYS_bsdthread_create || sysnum == SYS_vfork);
 #else
-    return (sysnum == SYS_vfork
-            IF_LINUX(|| (sysnum == SYS_clone && TEST(CLONE_VM, flags))));
+# ifdef SYS_vfork
+    if (sysnum == SYS_vfork)
+        return true;
+# endif
+# ifdef LINUX
+    if (sysnum == SYS_clone && TEST(CLONE_VM, flags))
+        return true;
+# endif
+    return false;
 #endif
 }
 
@@ -6291,6 +6343,7 @@ pre_system_call(dcontext_t *dcontext)
         }
         break;
     }
+# ifdef SYS_uselib
     case SYS_uselib: {
         /* Used to get the kernel to load a share library (legacy system call).
          * Was primarily used when statically linking to dynamically loaded shared
@@ -6300,6 +6353,7 @@ pre_system_call(dcontext_t *dcontext)
         ASSERT_NOT_IMPLEMENTED(false);
         break;
     }
+# endif
 #endif
 
     /****************************************************************************/
@@ -6380,6 +6434,7 @@ pre_system_call(dcontext_t *dcontext)
     }
 #endif
 
+#ifdef SYS_vfork
     case SYS_vfork: {
         /* treat as if sys_clone with flags just as sys_vfork does */
         /* in /usr/src/linux/arch/i386/kernel/process.c */
@@ -6396,11 +6451,11 @@ pre_system_call(dcontext_t *dcontext)
          */
         IF_LINUX(ASSERT(is_thread_create_syscall(dcontext)));
         dcontext->sys_param1 = mc->xsp; /* for restoring in parent */
-#ifdef MACOS
+# ifdef MACOS
         create_clone_record(dcontext, (reg_t *)&mc->xsp, NULL, NULL);
-#else
+# else
         create_clone_record(dcontext, (reg_t *)&mc->xsp /*child uses parent sp*/);
-#endif
+# endif
 
         /* We switch the lib tls segment back to app's segment.
          * Please refer to comment on os_switch_lib_tls.
@@ -6410,12 +6465,15 @@ pre_system_call(dcontext_t *dcontext)
         }
         break;
     }
+#endif
 
+#ifdef SYS_fork
     case SYS_fork: {
         LOG(THREAD, LOG_SYSCALLS, 2, "syscall: fork\n");
         os_fork_pre(dcontext);
         break;
     }
+#endif
 
     case SYS_execve: {
         handle_execve(dcontext);
@@ -6542,7 +6600,9 @@ pre_system_call(dcontext_t *dcontext)
         break;
     }
 #ifdef LINUX
+# ifdef SYS_signalfd
     case SYS_signalfd:         /* 282/321 */
+# endif
     case SYS_signalfd4: {      /* 289 */
         /* int signalfd (int fd, const sigset_t *mask, size_t sizemask) */
         /* int signalfd4(int fd, const sigset_t *mask, size_t sizemask, int flags) */
@@ -6550,9 +6610,11 @@ pre_system_call(dcontext_t *dcontext)
         dcontext->sys_param0 = sys_param(dcontext, 0);
         dcontext->sys_param1 = sys_param(dcontext, 1);
         dcontext->sys_param2 = sys_param(dcontext, 2);
+# ifdef SYS_signalfd
         if (dcontext->sys_num == SYS_signalfd)
             dcontext->sys_param3 = 0;
         else
+# endif
             dcontext->sys_param3 = sys_param(dcontext, 3);
         new_result =
             handle_pre_signalfd(dcontext, (int) dcontext->sys_param0,
@@ -6712,6 +6774,7 @@ pre_system_call(dcontext_t *dcontext)
         break;
     }
 
+#ifdef SYS_dup2
     case SYS_dup2:
     IF_LINUX(case SYS_dup3:) {
         file_t newfd = (file_t) sys_param(dcontext, 1);
@@ -6728,6 +6791,7 @@ pre_system_call(dcontext_t *dcontext)
         }
         break;
     }
+#endif
 
 #ifdef MACOS
     case SYS_fcntl_nocancel:
@@ -6830,6 +6894,7 @@ pre_system_call(dcontext_t *dcontext)
 #endif
 
 #ifdef LINUX
+# ifdef SYS_readlink
     case SYS_readlink:
         if (DYNAMO_OPTION(early_inject)) {
             dcontext->sys_param0 = sys_param(dcontext, 0);
@@ -6837,6 +6902,7 @@ pre_system_call(dcontext_t *dcontext)
             dcontext->sys_param2 = sys_param(dcontext, 2);
         }
         break;
+# endif
 
     /* i#107 syscalls that might change/query app's segment */
 
@@ -6917,10 +6983,12 @@ pre_system_call(dcontext_t *dcontext)
 # ifdef MACOS
     case SYS_open_nocancel:
 # endif
+# ifdef SYS_open
     case SYS_open: {
         dcontext->sys_param0 = sys_param(dcontext, 0);
         break;
     }
+# endif
 #endif
 
     default: {
@@ -7369,16 +7437,17 @@ post_system_call(dcontext_t *dcontext)
     }
 #endif
 
+#ifdef SYS_fork
     /* handle fork, try to do it early before too much logging occurs */
     if (sysnum == SYS_fork
         IF_LINUX(|| (sysnum == SYS_clone && !TEST(CLONE_VM, dcontext->sys_param0)))) {
         if (result == 0) {
             /* we're the child */
             thread_id_t child = get_sys_thread_id();
-#ifdef DEBUG
+# ifdef DEBUG
             thread_id_t parent = get_parent_id();
             SYSLOG_INTERNAL_INFO("-- parent %d forked child %d --", parent, child);
-#endif
+# endif
             /* first, fix TLS of dcontext */
             ASSERT(parent != 0);
             /* change parent pid to our pid */
@@ -7401,6 +7470,7 @@ post_system_call(dcontext_t *dcontext)
             os_fork_post(dcontext, true/*parent*/);
         }
     }
+#endif
 
 
     LOG(THREAD, LOG_SYSCALLS, 2,
@@ -7416,6 +7486,7 @@ post_system_call(dcontext_t *dcontext)
 # ifdef MACOS
     case SYS_open_nocancel:
 # endif
+# ifdef SYS_open
     case SYS_open: {
         if (success) {
             /* useful for figuring out what module was loaded that then triggers
@@ -7426,6 +7497,7 @@ post_system_call(dcontext_t *dcontext)
         }
         break;
     }
+# endif
 #endif
 
 #if defined(LINUX) && !defined(X64) && !defined(ARM)
@@ -7680,12 +7752,15 @@ post_system_call(dcontext_t *dcontext)
     }
 #endif
 
+#ifdef SYS_fork
     case SYS_fork: {
         LOG(THREAD, LOG_SYSCALLS, 2, "syscall: fork returned "PFX"\n",
             MCXT_SYSCALL_RES(mc));
         break;
     }
+#endif
 
+#ifdef SYS_vfork
     case SYS_vfork: {
         LOG(THREAD, LOG_SYSCALLS, 2, "syscall: vfork returned "PFX"\n",
             MCXT_SYSCALL_RES(mc));
@@ -7708,6 +7783,7 @@ post_system_call(dcontext_t *dcontext)
         }
         break;
     }
+#endif
 
     case SYS_execve: {
         /* if we get here it means execve failed (doesn't return on success) */
@@ -7804,14 +7880,16 @@ post_system_call(dcontext_t *dcontext)
     /****************************************************************************/
     /* FILES */
 
+#ifdef SYS_dup2
     case SYS_dup2:
     IF_LINUX(case SYS_dup3:) {
-#ifdef LINUX
+# ifdef LINUX
         if (success)
             signal_handle_dup(dcontext, (file_t) sys_param(dcontext, 1), (file_t) result);
-#endif
+# endif
         break;
     }
+#endif
 
 #ifdef MACOS
     case SYS_fcntl_nocancel:
@@ -7868,7 +7946,7 @@ post_system_call(dcontext_t *dcontext)
     }
 #endif
 
-#ifdef LINUX
+#ifdef SYS_readlink
     case SYS_readlink:
         if (success && DYNAMO_OPTION(early_inject)) {
             /* i#907: /proc/self/exe is a symlink to libdynamorio.so.  We need
@@ -8055,8 +8133,13 @@ read_proc_self_exe(bool ignore_cache)
         ASSERT(len > 0);
         NULL_TERMINATE_BUFFER(exepath);
         /* i#960: readlink does not null terminate, so we do it. */
+# ifdef SYS_readlink
         res = dynamorio_syscall(SYS_readlink, 3, exepath, exepath,
                                 BUFFER_SIZE_ELEMENTS(exepath)-1);
+# else
+        res = dynamorio_syscall(SYS_readlinkat, 4, AT_FDCWD, exepath, exepath,
+                                BUFFER_SIZE_ELEMENTS(exepath)-1);
+# endif
         ASSERT(res < BUFFER_SIZE_ELEMENTS(exepath));
         exepath[MAX(res, 0)] = '\0';
         NULL_TERMINATE_BUFFER(exepath);
@@ -8828,13 +8911,19 @@ wait_for_event(event_t e)
  * that as the next entry.
  */
 struct linux_dirent {
-    long           d_ino;       /* Inode number. */
-    off_t          d_off;       /* Offset to next linux_dirent. */
-    unsigned short d_reclen;    /* Length of this linux_dirent. */
-    char           d_name[];    /* File name, null-terminated. */
-#if 0  /* Has to be #if 0 because it's after the flexible array. */
-    char           d_pad;       /* Always zero? */
-    char           d_type;      /* File type, since Linux 2.6.4. */
+#ifdef SYS_getdents
+    /* Adapted from struct old_linux_dirent in linux/fs/readdir.c: */
+    unsigned long  d_ino;
+    unsigned long  d_off;
+    unsigned short d_reclen;
+    char           d_name[];
+#else
+    /* Adapted from struct linux_dirent64 in linux/include/linux/dirent.h: */
+    uint64         d_ino;
+    int64          d_off;
+    unsigned short d_reclen;
+    unsigned char  d_type;
+    char           d_name[];
 #endif
 };
 
@@ -8871,8 +8960,13 @@ os_dir_iterator_next(dir_iterator_t *iter)
          * the example code that this is based on.
          */
         iter->off = 0;
+# ifdef SYS_getdents
         iter->end = dynamorio_syscall(SYS_getdents, 3, iter->fd, iter->buf,
                                       sizeof(iter->buf));
+# else
+        iter->end = dynamorio_syscall(SYS_getdents64, 3, iter->fd, iter->buf,
+                                      sizeof(iter->buf));
+# endif
         ASSERT(iter->end <= sizeof(iter->buf));
         if (iter->end <= 0) {  /* No more dents, or error. */
             iter->name = NULL;

core/unix/include/syscall.h
@@ -20,8 +20,10 @@
 
 #ifdef X86
 # include "syscall_linux_x86.h"
-#elif defined(ARM) || defined(AARCH64)
+#elif defined(ARM)
 # include "syscall_linux_arm.h"
+#elif defined(AARCH64)
+# include "syscall_linux_arm.h" /* FIXME i#1569: "syscall_linux_uapi.h" */
 #endif
 
 #endif /* _SYSCALL_H_ */

core/unix/injector.c
@@ -1142,11 +1142,18 @@ injectee_open(dr_inject_info_t *info, const char *path, int flags, mode_t mode)
     opnd_t args[MAX_SYSCALL_ARGS];
     int num_args = 0;
     gen_push_string(dc, ilist, path);
+#ifndef SYS_open
+    args[num_args++] = OPND_CREATE_INTPTR(AT_FDCWD);
+#endif
     args[num_args++] = OPND_CREATE_MEMPTR(REG_XSP, 0);
     args[num_args++] = OPND_CREATE_INTPTR(flags);
     args[num_args++] = OPND_CREATE_INTPTR(mode);
     ASSERT(num_args <= MAX_SYSCALL_ARGS);
+#ifdef SYS_open
     gen_syscall(dc, ilist, SYSNUM_NO_CANCEL(SYS_open), num_args, args);
+#else
+    gen_syscall(dc, ilist, SYSNUM_NO_CANCEL(SYS_openat), num_args, args);
+#endif
     return injectee_run_get_retval(info, dc, ilist);
 }
 

core/unix/signal.c
@@ -776,10 +776,13 @@ signal_thread_inherit(dcontext_t *dcontext, void *clone_record)
         LOG(THREAD, LOG_ASYNCH, 1,
             "parent tid is "TIDFMT", parent sysnum is %d(%s), clone flags="PIFX"\n",
             record->caller_id, record->clone_sysnum,
+#ifdef SYS_vfork
             (record->clone_sysnum == SYS_vfork) ? "vfork" :
+#endif
             (IF_LINUX(record->clone_sysnum == SYS_clone ? "clone" :)
              IF_MACOS(record->clone_sysnum == SYS_bsdthread_create ? "bsdthread_create":)
              "unexpected"), record->clone_flags);
+#ifdef SYS_vfork
         if (record->clone_sysnum == SYS_vfork) {
             /* The above clone_flags argument is bogus.
                SYS_vfork doesn't have a free register to keep the hardcoded value
@@ -787,6 +790,7 @@ signal_thread_inherit(dcontext_t *dcontext, void *clone_record)
             /* CHECK: is this the only place real clone flags are needed? */
             record->clone_flags = CLONE_VFORK | CLONE_VM | SIGCHLD;
         }
+#endif
 
         /* handlers are either inherited or shared */
         if (TEST(CLONE_SIGHAND, record->clone_flags)) {

core/unix/signal_linux.c
@@ -221,12 +221,21 @@ sysnum_is_not_restartable(int sysnum)
      * + Socket interfaces: supposed to restart if no timeout has been set
      *   but we never restart for simplicity for now.
      */
-    return (sysnum == SYS_pause ||
+    return (
+#ifdef SYS_pause
+            sysnum == SYS_pause ||
+#endif
             sysnum == SYS_rt_sigsuspend ||
             sysnum == SYS_rt_sigtimedwait ||
             IF_X86_64(sysnum == SYS_epoll_wait_old ||)
-            sysnum == SYS_epoll_wait || sysnum == SYS_epoll_pwait ||
-            sysnum == SYS_poll || sysnum == SYS_ppoll ||
+#ifdef SYS_epoll_wait
+            sysnum == SYS_epoll_wait ||
+#endif
+            sysnum == SYS_epoll_pwait ||
+#ifdef SYS_poll
+            sysnum == SYS_poll ||
+#endif
+            sysnum == SYS_ppoll ||
             IF_X86(sysnum == SYS_select ||)
             sysnum == SYS_pselect6 ||
 #ifdef X64

core/unix/stackdump.c
@@ -140,7 +140,11 @@ fork_syscall(void)
     return fork();
 # endif
 #else
+# ifdef SYS_fork
     return dynamorio_syscall(SYS_fork, 0);
+# else
+    return dynamorio_syscall(SYS_clone, 5, SIGCHLD, NULL, NULL, NULL, NULL);
+# endif
 #endif
 }
 

commit 38876125e3b76f7ce92c84d9f43aa28a2755c480

Fix Mac build breakage.
Fix build breakage on Mac from c4b55cb.

Review-URL: https://codereview.appspot.com/292960043

Match-id-795a6024c511ddf8329c4855e5b4d9391ed43e18

core/unix/os.c
@@ -7946,7 +7946,7 @@ post_system_call(dcontext_t *dcontext)
     }
 #endif
 
-#ifdef SYS_readlink
+#if defined(LINUX) && defined(SYS_readlink)
     case SYS_readlink:
         if (success && DYNAMO_OPTION(early_inject)) {
             /* i#907: /proc/self/exe is a symlink to libdynamorio.so.  We need