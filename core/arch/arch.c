ptrbman/dynamorio

commit 71b0060ea19a18245813e585174c678cab8daf50

i#1921 native signals: remove handlers and hooks while fully native
Adds removal of all signal handler and vsyscall hooks while fully native
(during usage of the start/stop API).  This simplifies handling of native
threads.  This does not yet solve single-native-thread signals for
native_exec or DR_EMIT_GO_NATIVE.

The implementation uses a new routine os_process_{not_,}under_dynamorio().

A test api.static_signal is forthcoming but it still requires some other DR
attach fixes before it fully works.

Review-URL: https://codereview.appspot.com/308680043

core/arch/arch.c
@@ -2778,8 +2778,8 @@ unhook_vsyscall(void)
 
 #define VSYS_DISPLACED_LEN 4
 
-static bool
-hook_vsyscall(dcontext_t *dcontext)
+bool
+hook_vsyscall(dcontext_t *dcontext, bool method_changing)
 {
 #ifdef X86
     bool res = true;
@@ -2788,8 +2788,14 @@ hook_vsyscall(dcontext_t *dcontext)
     uint num_nops = 0;
     uint prot;
 
+# ifdef X64
+    return false;
+# endif
+    /* On a call on a method change the method is not yet finalized so we always try */
+    if (get_syscall_method() != SYSCALL_METHOD_SYSENTER && !method_changing)
+        return false;
+
     ASSERT(DATASEC_WRITABLE(DATASEC_RARELY_PROT));
-    IF_X64(ASSERT_NOT_REACHED()); /* no sysenter support on x64 */
     ASSERT(vsyscall_page_start != NULL && vsyscall_syscall_end_pc != NULL &&
            vsyscall_page_start == (app_pc)PAGE_START(vsyscall_syscall_end_pc));
 
@@ -2894,8 +2900,10 @@ hook_vsyscall(dcontext_t *dcontext)
     return res;
 # undef CHECK
 #elif defined(AARCHXX)
-    /* No vsyscall support needed for our ARM targets */
-    ASSERT_NOT_REACHED();
+    /* No vsyscall support needed for our ARM targets -- still called on
+     * os_process_under_dynamorio().
+     */
+    ASSERT(!method_changing);
     return false;
 #endif /* X86/ARM */
 }
@@ -3065,7 +3073,7 @@ check_syscall_method(dcontext_t *dcontext, instr_t *instr)
             }
 #  endif
             /* Hook the sysenter continuation point so we don't lose control */
-            if (!sysenter_hook_failed && !hook_vsyscall(dcontext)) {
+            if (!sysenter_hook_failed && !hook_vsyscall(dcontext, true/*force*/)) {
                 /* PR 212570: for now we bail out to using int;
                  * for performance we should clobber the retaddr and
                  * keep the sysenters.

core/arch/arch_exports.h
@@ -1079,6 +1079,8 @@ bool does_syscall_ret_to_callsite(void);
 void set_syscall_method(int method);
 #ifdef LINUX
 bool should_syscall_method_be_sysenter(void);
+bool hook_vsyscall(dcontext_t *dcontext, bool method_changing);
+bool unhook_vsyscall(void);
 #endif
 /* returns the address of the first app syscall instruction we saw (see hack
  * in win32/os.c that uses this for PRE_SYSCALL_PC, not for general use */

core/arch/interp.c
@@ -4089,7 +4089,7 @@ build_bb_ilist(dcontext_t *dcontext, build_bb_t *bb)
         /* i#1582: required for now on ARM */
         IF_UNIX(os_swap_context_go_native(dcontext, 0));
         /* i#1921: for now we do not support re-attach, so remove handlers */
-        IF_UNIX(signal_remove_handlers(dcontext));
+        os_process_not_under_dynamorio(dcontext);
         bb_build_abort(dcontext, true/*free vmlist*/, false/*don't unlock*/);
         return;
     }

core/dynamo.c
@@ -2556,8 +2556,14 @@ dr_app_setup(void)
      * We should share the suspend-and-takeover loop (and for dr_app_setup_and_start
      * share the takeover portion) from dr_app_start().
      */
+    int res;
     dr_api_entry = true;
-    return dynamorio_app_init();
+    res = dynamorio_app_init();
+    /* It would be more efficient to avoid setting up signal handlers and
+     * avoid hooking vsyscall during init, but the code is simpler this way.
+     */
+    os_process_not_under_dynamorio(get_thread_private_dcontext());
+    return res;
 }
 
 /* API routine to exit DR */
@@ -2565,6 +2571,9 @@ DR_APP_API int
 dr_app_cleanup(void)
 {
     thread_record_t *tr;
+    SELF_UNPROTECT_DATASEC(DATASEC_RARELY_PROT);
+    dr_api_exit = true;
+    SELF_PROTECT_DATASEC(DATASEC_RARELY_PROT); /* to keep properly nested */
 
     /* XXX: The dynamo_thread_[not_]under_dynamo() routines are not idempotent,
      * and must be balanced!  On Linux, they track the shared itimer refcount,
@@ -2577,12 +2586,10 @@ dr_app_cleanup(void)
      * dynamo_thread_not_under_dynamo() about updating tr->under_dynamo_control.
      */
     tr = thread_lookup(get_thread_id());
-    if (tr != NULL && tr->dcontext != NULL)
+    if (tr != NULL && tr->dcontext != NULL) {
+        os_process_under_dynamorio(tr->dcontext);
         dynamo_thread_under_dynamo(tr->dcontext);
-
-    SELF_UNPROTECT_DATASEC(DATASEC_RARELY_PROT);
-    dr_api_exit = true;
-    SELF_PROTECT_DATASEC(DATASEC_RARELY_PROT); /* to keep properly nested */
+    }
     return dynamorio_app_exit();
 }
 
@@ -2690,6 +2697,7 @@ dynamorio_take_over_threads(dcontext_t *dcontext)
     bool found_threads;
     uint attempts = 0;
 
+    os_process_under_dynamorio(dcontext);
     /* XXX i#1305: we should suspend all the other threads for DR init to
      * satisfy the parts of the init process that assume there are no races.
      */

core/os_shared.h
@@ -64,6 +64,8 @@ void os_thread_exit(dcontext_t *dcontext, bool other_thread);
 void os_thread_under_dynamo(dcontext_t *dcontext);
 /* must only be called for the executing thread */
 void os_thread_not_under_dynamo(dcontext_t *dcontext);
+void os_process_under_dynamorio(dcontext_t *dcontext);
+void os_process_not_under_dynamorio(dcontext_t *dcontext);
 
 bool os_take_over_all_unknown_threads(dcontext_t *dcontext);
 

core/synch.c
@@ -1823,6 +1823,8 @@ send_all_other_threads_native(void)
 
     end_synch_with_all_threads(threads, num_threads, true/*resume*/);
 
+    os_process_not_under_dynamorio(my_dcontext);
+
     if (waslinking)
         enter_couldbelinking(my_dcontext, NULL, false);
     return;

core/unix/os.c
@@ -2293,6 +2293,24 @@ os_thread_not_under_dynamo(dcontext_t *dcontext)
     os_swap_context(dcontext, true/*to app*/, DR_STATE_ALL);
 }
 
+void
+os_process_under_dynamorio(dcontext_t *dcontext)
+{
+    LOG(GLOBAL, LOG_THREADS, 1, "process now under DR\n");
+    /* We only support regular process-wide signal handlers for delayed takeover. */
+    signal_reinstate_handlers(dcontext);
+    hook_vsyscall(dcontext, false);
+}
+
+void
+os_process_not_under_dynamorio(dcontext_t *dcontext)
+{
+    /* We only support regular process-wide signal handlers for mixed-mode control. */
+    signal_remove_handlers(dcontext);
+    unhook_vsyscall();
+    LOG(GLOBAL, LOG_THREADS, 1, "process no longer under DR\n");
+}
+
 bool
 detach_do_not_translate(thread_record_t *tr)
 {
@@ -9225,6 +9243,8 @@ os_take_over_all_unknown_threads(dcontext_t *dcontext)
             /* Re-takeover known threads that are currently native as well.
              * XXX i#95: we need a synchall-style loop for known threads as
              * they can be in DR for syscall hook handling.
+             * Update: we now remove the hook for start/stop: but native_exec
+             * or other individual threads going native could still hit this.
              */
             (is_thread_currently_native(tr)
              IF_CLIENT_INTERFACE(&& !IS_CLIENT_THREAD(tr->dcontext))))

core/unix/os_exports.h
@@ -304,9 +304,6 @@ os_handle_mov_seg(dcontext_t *dcontext, byte *pc);
 
 void init_emulated_brk(app_pc exe_end);
 
-/* in arch.c */
-bool unhook_vsyscall(void);
-
 /***************************************************************************/
 /* in signal.c */
 
@@ -409,6 +406,9 @@ signal_fork_init(dcontext_t *dcontext);
 
 void
 signal_remove_handlers(dcontext_t *dcontext);
+
+void
+signal_reinstate_handlers(dcontext_t *dcontext);
 
 bool
 set_itimer_callback(dcontext_t *dcontext, int which, uint millisec,

commit 0c6cebb7d0a38c723196623a9018f1afe173001d

Fix Mac breakage from 71b0060
Fixes Mac OSX build breakage from 71b0060 regarding i#1921
{,un}hook_vsyscall.

Review-URL: https://codereview.appspot.com/312830043

Match-id-c8e3bd3ed22ec314a779953eb9ad465e997acd19

core/arch/arch.c
@@ -2714,6 +2714,11 @@ get_cleanup_and_terminate_global_do_syscall_entry()
  * the caller's retaddr in edx.  Thus, there is nothing to hook.
  */
 bool
+hook_vsyscall(dcontext_t *dcontext, bool method_changing)
+{
+    return false;
+}
+bool
 unhook_vsyscall(void)
 {
     return false;

core/arch/arch_exports.h
@@ -1079,9 +1079,9 @@ bool does_syscall_ret_to_callsite(void);
 void set_syscall_method(int method);
 #ifdef LINUX
 bool should_syscall_method_be_sysenter(void);
+#endif
 bool hook_vsyscall(dcontext_t *dcontext, bool method_changing);
 bool unhook_vsyscall(void);
-#endif
 /* returns the address of the first app syscall instruction we saw (see hack
  * in win32/os.c that uses this for PRE_SYSCALL_PC, not for general use */
 byte *get_app_sysenter_addr(void);