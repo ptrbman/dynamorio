ptrbman/dynamorio

commit 354960af363c98bdf8b68c20823131323b0ce4a1

i#1729 offline traces: add timestamps and thread interleaving
Adds a new timestamp header for each buffer unit written to an offline
trace file.  The timestamp uses DR's new dr_get_microseconds() for a simple
cross-platform implementation.

Adds interleaving of threads in timestamp order to the raw2trace
post-processor.

Adds a test "burst-threads" which tests quite a few things:
+ Starting and stopping from a different thread than called dr_app_setup
  (i#2034, i#1921)
+ Detaching with many threads on Linux (i#2036)
+ Stress test for state translation with a heavyweight client (found
  i#2035)

The test is temporarily disabled on Windows until i#2040 is fixed.

The test produces such large trace files (in the other threads that keep
running while being synched for detach) that we cap their size with
-max_trace_size to keep this a small enough test for the regression suite.

Review-URL: https://codereview.appspot.com/317750043

clients/drcachesim/tracer/raw2trace.cpp
@@ -247,7 +247,7 @@ raw2trace_t::append_memref(trace_entry_t *buf_in, uint tidx, instr_t *instr,
         // They could be earlier, so "instr" may not itself be predicated.
         // XXX i#2015: if there are multiple predicated memrefs, our instr vs
         // data stream may not be in the correct order here.
-        VPRINT(3, "Missing memref (next type is 0x" ZHEX64_FORMAT_STRING ")\n",
+        VPRINT(4, "Missing memref (next type is 0x" ZHEX64_FORMAT_STRING ")\n",
                in_entry.combined_value);
         // Put back the entry.
         thread_files[tidx]->seekg(-(std::streamoff)sizeof(in_entry),
@@ -269,7 +269,7 @@ raw2trace_t::append_memref(trace_entry_t *buf_in, uint tidx, instr_t *instr,
     }
     // We take the full value, to handle low or high.
     buf->addr = (addr_t) in_entry.combined_value;
-    VPRINT(3, "Appended memref to " PFX "\n", (ptr_uint_t)buf->addr);
+    VPRINT(4, "Appended memref to " PFX "\n", (ptr_uint_t)buf->addr);
     ++buf;
     return buf;
 }
@@ -287,10 +287,10 @@ raw2trace_t::append_bb_entries(uint tidx, offline_entry_t *in_entry)
         // FIXME i#1729: add support for code not in a module (vsyscall, JIT, etc.).
         // Once that support is in we can remove the bool return value and handle
         // the memrefs up here.
-        VPRINT(2, "Skipping ifetch for %u instrs not in a module\n", instr_count);
+        VPRINT(3, "Skipping ifetch for %u instrs not in a module\n", instr_count);
         return false;
     } else {
-        VPRINT(2, "Appending %u instrs in bb " PFX " in mod %u +" PIFX " = %s\n",
+        VPRINT(3, "Appending %u instrs in bb " PFX " in mod %u +" PIFX " = %s\n",
                instr_count, (ptr_uint_t)start_pc, (uint)in_entry->pc.modidx,
                (ptr_uint_t)in_entry->pc.modoffs, modvec[in_entry->pc.modidx].path);
     }
@@ -349,34 +349,71 @@ raw2trace_t::append_bb_entries(uint tidx, offline_entry_t *in_entry)
 void
 raw2trace_t::merge_and_process_thread_files()
 {
-    uint tidx = 0;
+    // The current thread we're processing is tidx.  If it's set to thread_files.size()
+    // that means we need to pick a new thread.
+    uint tidx = (uint)thread_files.size();
     uint thread_count = (uint)thread_files.size();
     offline_entry_t in_entry;
     online_instru_t instru(NULL);
     bool last_bb_handled = true;
     std::vector<thread_id_t> tids(thread_files.size(), INVALID_THREAD_ID);
-    byte buf[MAX_COMBINED_ENTRIES * sizeof(trace_entry_t)];
+    std::vector<uint64> times(thread_files.size(), 0);
+    byte buf_base[MAX_COMBINED_ENTRIES * sizeof(trace_entry_t)];
 
     // We read the thread files simultaneously in lockstep and merge them into
     // a single output file in timestamp order.
+    // When a thread file runs out we leave its times[] entry as 0 and its file at eof.
     // We convert each offline entry into a trace_entry_t.
     // We fill in instr entries and memref type and size.
     do {
         int size = 0;
+        byte *buf = buf_base;
+        if (tidx >= thread_files.size()) {
+            // Pick the next thread by looking for the smallest timestamp.
+            uint64 min_time = 0xffffffffffffffff;
+            uint next_tidx = 0;
+            for (uint i=0; i<times.size(); ++i) {
+                if (times[i] == 0 && !thread_files[i]->eof()) {
+                    offline_entry_t entry;
+                    if (!thread_files[i]->read((char*)&entry, sizeof(entry)))
+                        FATAL_ERROR("Failed to read from input file");
+                    if (entry.timestamp.type != OFFLINE_TYPE_TIMESTAMP)
+                        FATAL_ERROR("Missing timestamp entry");
+                    times[i] = entry.timestamp.usec;
+                    VPRINT(3, "Thread %u timestamp is @0x" ZHEX64_FORMAT_STRING
+                           "\n", (uint)tids[i], times[i]);
+                }
+                if (times[i] != 0 && times[i] < min_time) {
+                    min_time = times[i];
+                    next_tidx = i;
+                }
+            }
+            VPRINT(2, "Next thread in timestamp order is %u @0x" ZHEX64_FORMAT_STRING
+                   "\n", (uint)tids[next_tidx], times[next_tidx]);
+            tidx = next_tidx;
+            times[tidx] = 0; // Read from file for this thread's next timestamp.
+            size += instru.append_tid(buf, tids[tidx]);
+            buf += size;
+        }
         if (!thread_files[tidx]->read((char*)&in_entry, sizeof(in_entry))) {
             if (thread_files[tidx]->eof()) {
                 CHECK(tids[tidx] != INVALID_THREAD_ID, "Missing thread id");
-                VPRINT(2, "Thread %d exit\n", (int)tids[tidx]);
-                size = instru.append_thread_exit(buf, tids[tidx]);
+                VPRINT(2, "Thread %d exit\n", (uint)tids[tidx]);
+                size += instru.append_thread_exit(buf, tids[tidx]);
+                buf += size;
                 --thread_count;
                 if (thread_count == 0)
                     break;
-                // FIXME i#1729: pick new thread based on timestamps
+                tidx = (uint)thread_files.size(); // Request thread scan.
+                continue;
             } else
                 FATAL_ERROR("Failed to read from input file");
         }
         if (in_entry.timestamp.type == OFFLINE_TYPE_TIMESTAMP) {
-            // FIXME i#1729: pick new thread based on timestamps
+            VPRINT(2, "Thread %u timestamp 0x" ZHEX64_FORMAT_STRING "\n",
+                   tids[tidx], in_entry.timestamp.usec);
+            times[tidx] = in_entry.timestamp.usec;
+            tidx = (uint)thread_files.size(); // Request thread scan.
         } else if (in_entry.addr.type == OFFLINE_TYPE_MEMREF ||
                    in_entry.addr.type == OFFLINE_TYPE_MEMREF_HIGH) {
             if (!last_bb_handled) {
@@ -386,9 +423,10 @@ raw2trace_t::merge_and_process_thread_files()
                 entry->type = TRACE_TYPE_READ; // Guess.
                 entry->size = 1; // Guess.
                 entry->addr = (addr_t) in_entry.combined_value;
-                VPRINT(3, "Appended non-module memref to " PFX "\n",
+                VPRINT(4, "Appended non-module memref to " PFX "\n",
                        (ptr_uint_t)entry->addr);
-                size = sizeof(*entry);
+                size += sizeof(*entry);
+                buf += size;
             } else {
                 // We should see an instr entry first
                 CHECK(false, "memref entry found outside of bb");
@@ -399,15 +437,17 @@ raw2trace_t::merge_and_process_thread_files()
             VPRINT(2, "Thread %u entry\n", (uint)in_entry.tid.tid);
             if (tids[tidx] == INVALID_THREAD_ID)
                 tids[tidx] = in_entry.tid.tid;
-            size = instru.append_tid(buf, in_entry.tid.tid);
+            size += instru.append_tid(buf, in_entry.tid.tid);
+            buf += size;
         } else if (in_entry.pid.type == OFFLINE_TYPE_PID) {
             VPRINT(2, "Process %u entry\n", (uint)in_entry.pid.pid);
-            size = instru.append_pid(buf, in_entry.pid.pid);
+            size += instru.append_pid(buf, in_entry.pid.pid);
+            buf += size;
         } else
-            FATAL_ERROR("Unknown trace type %d", in_entry.timestamp.type);
+            FATAL_ERROR("Unknown trace type %d", (int)in_entry.timestamp.type);
         if (size > 0) {
             CHECK((uint)size < MAX_COMBINED_ENTRIES, "Too many entries");
-            if (!out_file.write((char*)buf, size))
+            if (!out_file.write((char*)buf_base, size))
                 FATAL_ERROR("Failed to write to output file");
         }
     } while (thread_count > 0);

CMakeLists.txt
@@ -1583,6 +1583,40 @@ if (CLIENT_INTERFACE OR APP_EXPORTS)
     REGEX "annotations/valgrind.h|annotations/memcheck.h" EXCLUDE)
 endif (CLIENT_INTERFACE OR APP_EXPORTS)
 
+# Used by tests in multiple places.
+if (BUILD_TESTS AND UNIX)
+  if (ANDROID)
+    # pthreads is inside Bionic on Android, and for some reason find_library() can't
+    # locate libm or libdl within the Android toolchain: we just assume they're there.
+    set(libmath m)
+    set(libdl dl)
+    set(libpthread "")
+  else ()
+    # i#720: cmake fails to find 32-bit libraries in Ubuntu 11.10.
+    # This is because cmake uses CMAKE_LIBRARY_ARCHITECTURE to handle
+    # multi-arch compilation, whose value is x86_64-linux-gnu or i386-linux-gnu
+    # for 64/32-bit. However, in Ubuntu 11.10, some of the 32-bit libraries
+    # like libm.so, libdl.so, and libpthread.so are in /usr/lib32 or /lib32
+    # instead of /lib/i386-linux-gnu, so cmake cannot to find them.
+    # solution: add explicit paths for lookup.
+    find_library(libmath m)
+    if (NOT libmath AND NOT X64)
+      find_library(libmath m PATHS /usr/lib32 /lib32)
+    endif ()
+    find_library(libdl dl)
+    if (NOT libdl AND NOT X64)
+      find_library(libdl dl PATHS /usr/lib32 /lib32)
+    endif ()
+    find_library(libpthread pthread)
+    if (NOT libpthread AND NOT X64)
+      find_library(libpthread pthread PATHS /usr/lib32 /lib32)
+    endif ()
+    if (NOT libmath OR NOT libdl OR NOT libpthread)
+      message(FATAL_ERROR "cannot find required libs m, dl, and/or pthread")
+    endif ()
+  endif ()
+endif ()
+
 if (BUILD_CLIENTS)
   # Must be prior to api/docs
   add_subdirectory(clients)

clients/drcachesim/CMakeLists.txt
@@ -235,13 +235,21 @@ endif ()
 # We build larger executables here.  All tests are added in suite/tests/.
 # Be sure to give the targets qualified test names ("tool.drcache*...").
 
-# FIXME i#2007: fails to link on A64
-# XXX i#1997: dynamorio_static is not supported on Mac yet
-if (NOT AARCH64 AND NOT APPLE)
-  add_executable(tool.drcacheoff.burst_static tests/burst_static.cpp)
-  configure_DynamoRIO_static(tool.drcacheoff.burst_static)
-  use_DynamoRIO_static_client(tool.drcacheoff.burst_static drmemtrace_static)
-  add_win32_flags(tool.drcacheoff.burst_static)
+if (BUILD_TESTS)
+  # FIXME i#2007: fails to link on A64
+  # XXX i#1997: dynamorio_static is not supported on Mac yet
+  if (NOT AARCH64 AND NOT APPLE)
+    add_executable(tool.drcacheoff.burst_static tests/burst_static.cpp)
+    configure_DynamoRIO_static(tool.drcacheoff.burst_static)
+    use_DynamoRIO_static_client(tool.drcacheoff.burst_static drmemtrace_static)
+    add_win32_flags(tool.drcacheoff.burst_static)
+
+    add_executable(tool.drcacheoff.burst_threads tests/burst_threads.cpp)
+    configure_DynamoRIO_static(tool.drcacheoff.burst_threads)
+    use_DynamoRIO_static_client(tool.drcacheoff.burst_threads drmemtrace_static)
+    add_win32_flags(tool.drcacheoff.burst_threads)
+    target_link_libraries(tool.drcacheoff.burst_threads ${libpthread})
+  endif ()
 endif ()
 
 ##################################################

clients/drcachesim/common/trace_entry.h
@@ -192,9 +192,8 @@ struct _offline_entry_t {
             uint64_t type:3;
         } pid;
         struct {
-            uint32_t tv_sec;
-            uint32_t tv_usec:29; // We only need 20 bits here.
-            uint32_t type:3;
+            uint64_t usec:61; // Microseconds since Jan 1, 1601.
+            uint64_t type:3;
         } timestamp;
         uint64_t combined_value;
         // XXX: add a CPU id entry for more faithful thread scheduling.

clients/drcachesim/tests/burst_threads.cpp
@@ -1,0 +1,151 @@
+/* **********************************************************
+ * Copyright (c) 2016 Google, Inc.  All rights reserved.
+ * **********************************************************/
+
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Google, Inc. nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL GOOGLE, INC. OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+/* This application links in drmemtrace_static and acquires a trace during
+ * a "burst" of execution in the middle of the application.  It then detaches.
+ */
+
+/* We deliberately do not include configure.h here to simulate what an
+ * actual app will look like.  configure_DynamoRIO_static sets DR_APP_EXPORTS
+ * for us.
+ */
+#include "dr_api.h"
+#include <assert.h>
+#include <iostream>
+#include <math.h>
+#include <stdint.h>
+#include <stdlib.h>
+#ifndef UNIX
+# include <process.h>
+#endif
+
+static const int num_threads = 8;
+static const int burst_owner = 4;
+static bool finished[num_threads];
+
+bool
+my_setenv(const char *var, const char *value)
+{
+#ifdef UNIX
+    return setenv(var, value, 1/*override*/) == 0;
+#else
+    return SetEnvironmentVariable(var, value) == TRUE;
+#endif
+}
+
+static int
+do_some_work(int i)
+{
+    static int iters = 512;
+    double val = (double)i;
+    for (int i = 0; i < iters; ++i) {
+        val += sin(val);
+    }
+    return (val > 0);
+}
+
+#ifdef WINDOWS
+unsigned int __stdcall
+#else
+void *
+#endif
+thread_func(void *arg)
+{
+    unsigned int idx = (unsigned int)(uintptr_t)arg;
+    static const int outer_iters = 2048;
+    /* We trace a 4-iter burst of execution. */
+    static const int iter_start = outer_iters/3;
+    static const int iter_stop = iter_start + 4;
+
+    for (int i = 0; i < outer_iters; ++i) {
+        if (idx == burst_owner && i == iter_start) {
+            std::cerr << "pre-DR start\n";
+            dr_app_start();
+        }
+        if (idx == burst_owner) {
+            if (i >= iter_start && i <= iter_stop)
+                assert(dr_app_running_under_dynamorio());
+            else
+                assert(!dr_app_running_under_dynamorio());
+        }
+        if (do_some_work(i) < 0)
+            std::cerr << "error in computation\n";
+        if (idx == burst_owner && i == iter_stop) {
+            std::cerr << "pre-DR detach\n";
+            dr_app_stop_and_cleanup();
+        }
+    }
+    finished[idx] = true;
+    return 0;
+}
+
+int
+main(int argc, const char *argv[])
+{
+#ifdef UNIX
+    pthread_t thread[num_threads];
+#else
+    uintptr_t thread[num_threads];
+#endif
+
+    /* While the start/stop thread only runs 4 iters, the other threads end up
+     * running more and their trace files get up to 65MB or more, with the
+     * merged result several GB's: too much for a test.  We thus cap each thread.
+     */
+    if (!my_setenv("DYNAMORIO_OPTIONS", "-stderr_mask 0xc -client_lib ';;-offline -max_trace_size 256K'"))
+        std::cerr << "failed to set env var!\n";
+
+    std::cerr << "pre-DR init\n";
+    dr_app_setup();
+    assert(!dr_app_running_under_dynamorio());
+
+    for (uint i = 0; i < num_threads; i++) {
+#ifdef UNIX
+        pthread_create(&thread[i], NULL, thread_func, (void*)(uintptr_t)i);
+#else
+        thread[i] = _beginthreadex(NULL, 0, thread_func, (void*)(uintptr_t)i, 0, NULL);
+#endif
+    }
+    for (uint i = 0; i < num_threads; i++) {
+#ifdef UNIX
+        pthread_join(thread[i], NULL);
+#else
+        WaitForSingleObject((HANDLE)thread[i], INFINITE);
+#endif
+    }
+    for (int i = 0; i < num_threads; ++i) {
+        if (!finished[i])
+            std::cerr << "thread " << i << " failed to finish\n";
+    }
+    std::cerr << "all done\n";
+    return 0;
+}

clients/drcachesim/tests/offline-burst_threads.templatex
@@ -1,0 +1,46 @@
+pre-DR init
+pre-DR start
+pre-DR detach
+all done
+Core #0 \(3 thread\(s\)\)
+  L1I stats:
+    Hits:                         *[0-9,\.]*...
+    Misses:                       *[0-9,\.]*.
+.*    Miss rate:                  *[0-9,\.]*%
+  L1D stats:
+    Hits:                         *[0-9,\.]*...
+    Misses:                       *[0-9,\.]*.
+.*   Miss rate:                   *[0-9,\.]*%
+Core #1 \(2 thread\(s\)\)
+  L1I stats:
+    Hits:                         *[0-9,\.]*...
+    Misses:                       *[0-9,\.]*.
+.*    Miss rate:                  *[0-9,\.]*%
+  L1D stats:
+    Hits:                         *[0-9,\.]*...
+    Misses:                       *[0-9,\.]*.
+.*   Miss rate:                   *[0-9,\.]*%
+Core #2 \(2 thread\(s\)\)
+  L1I stats:
+    Hits:                         *[0-9,\.]*...
+    Misses:                       *[0-9,\.]*.
+.*    Miss rate:                  *[0-9,\.]*%
+  L1D stats:
+    Hits:                         *[0-9,\.]*...
+    Misses:                       *[0-9,\.]*.
+.*   Miss rate:                   *[0-9,\.]*%
+Core #3 \(2 thread\(s\)\)
+  L1I stats:
+    Hits:                         *[0-9,\.]*...
+    Misses:                       *[0-9,\.]*.
+.*    Miss rate:                  *[0-9,\.]*%
+  L1D stats:
+    Hits:                         *[0-9,\.]*...
+    Misses:                       *[0-9,\.]*.
+.*   Miss rate:                   *[0-9,\.]*%
+LL stats:
+    Hits:                         *[0-9,\.]*.
+    Misses:                       *[0-9,\.]*..
+.*   Local miss rate:             *[0-9,\.]*%
+    Child hits:                   *[0-9,\.]*...
+    Total miss rate:              *[0-9,\.]*%

clients/drcachesim/tracer/instru.h
@@ -65,6 +65,8 @@ public:
     virtual int append_tid(byte *buf_ptr, thread_id_t tid) = 0;
     virtual int append_thread_exit(byte *buf_ptr, thread_id_t tid) = 0;
     virtual int append_iflush(byte *buf_ptr, addr_t start, size_t size) = 0;
+    // This is a per-buffer-writeout header.
+    virtual int append_header(byte *buf_ptr, thread_id_t tid) = 0;
 
     // These insert inlined code to add an entry into the trace buffer.
     virtual int instrument_memref(void *drcontext, instrlist_t *ilist, instr_t *where,
@@ -110,6 +112,7 @@ public:
     virtual int append_tid(byte *buf_ptr, thread_id_t tid);
     virtual int append_thread_exit(byte *buf_ptr, thread_id_t tid);
     virtual int append_iflush(byte *buf_ptr, addr_t start, size_t size);
+    virtual int append_header(byte *buf_ptr, thread_id_t tid);
 
     virtual int instrument_memref(void *drcontext, instrlist_t *ilist, instr_t *where,
                                   reg_id_t reg_ptr, reg_id_t reg_tmp, int adjust,
@@ -154,6 +157,7 @@ public:
     virtual int append_tid(byte *buf_ptr, thread_id_t tid);
     virtual int append_thread_exit(byte *buf_ptr, thread_id_t tid);
     virtual int append_iflush(byte *buf_ptr, addr_t start, size_t size);
+    virtual int append_header(byte *buf_ptr, thread_id_t tid);
 
     virtual int instrument_memref(void *drcontext, instrlist_t *ilist, instr_t *where,
                                   reg_id_t reg_ptr, reg_id_t reg_tmp, int adjust,

clients/drcachesim/tracer/instru_offline.cpp
@@ -143,6 +143,19 @@ offline_instru_t::append_iflush(byte *buf_ptr, addr_t start, size_t size)
     return 2 * sizeof(offline_entry_t);
 }
 
+int
+offline_instru_t::append_header(byte *buf_ptr, thread_id_t tid)
+{
+    offline_entry_t *entry = (offline_entry_t *) buf_ptr;
+    entry->timestamp.type = OFFLINE_TYPE_TIMESTAMP;
+    // We use dr_get_microseconds() for a simple, cross-platform implementation.
+    // This is once per buffer write, so a syscall here should be ok.
+    // If we want something faster we can try to use the VDSO gettimeofday (via
+    // libc) or KUSER_SHARED_DATA on Windows.
+    entry->timestamp.usec = dr_get_microseconds();
+    return sizeof(offline_entry_t);
+}
+
 void
 offline_instru_t::insert_save_pc(void *drcontext, instrlist_t *ilist, instr_t *where,
                                  reg_id_t reg_ptr, reg_id_t scratch, int adjust,

commit 445e415e4028fa2a4e1e405d02a88cbdd8349ca5

Fix build failure introduced in 354960a
Cast tids[tidx] to uint for printing.

Review-URL: https://codereview.appspot.com/313900043

Match-id-926b8ebd43ee5ef22a2e4f75105bfec11b2914c3

clients/drcachesim/tracer/raw2trace.cpp
@@ -411,7 +411,7 @@ raw2trace_t::merge_and_process_thread_files()
         }
         if (in_entry.timestamp.type == OFFLINE_TYPE_TIMESTAMP) {
             VPRINT(2, "Thread %u timestamp 0x" ZHEX64_FORMAT_STRING "\n",
-                   tids[tidx], in_entry.timestamp.usec);
+                   (uint)tids[tidx], in_entry.timestamp.usec);
             times[tidx] = in_entry.timestamp.usec;
             tidx = (uint)thread_files.size(); // Request thread scan.
         } else if (in_entry.addr.type == OFFLINE_TYPE_MEMREF ||