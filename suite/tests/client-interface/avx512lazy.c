ptrbman/dynamorio

commit 6ca8d4a9d81d481408652f6d3fd16a8d55080516

i#1312 AVX-512 support: Add get_zmm_caller_saved raw bytes function. (#3435)
This patch does nothing else but adding a AVX-512 zmm version of get_ymm_caller_saved encoded in raw bytes. The function is not used by DynamoRIO's core yet. The MCXT_SIMD_SLOT_SIZE is assumed to be 64 bytes. Using this function will require a future patch that changes DynamoRIO's slot size first.

With regards to #3422, no special 'ifdef' case for Windows has been added nor does this patch address the issue.

The comment has been modified for why we are encoding in raw bytes instead of mnemonics.

x86 unit tests have been added for get_ymm_caller_saved() and get_zmm_caller_saved().

Issue: #1312

CMakeLists.txt
@@ -1422,8 +1422,11 @@ if (BUILD_TESTS)
   # Tests require tools
   set(BUILD_TOOLS ON)
   enable_testing()
+  set(proc_supports_avx 0FF)
+  set(proc_supports_avx512 0FF)
   if (UNIX)
-    # Not used yet, testing AVX queries here.
+    set(CFLAGS_AVX "-mavx")
+    set(CFLAGS_AVX512 "-mavx512f")
     check_avx_processor_and_compiler_support(proc_supports_avx)
     check_avx512_processor_and_compiler_support(proc_supports_avx512)
   endif ()

core/arch/arch.h
@@ -1327,6 +1327,8 @@ void
 get_xmm_caller_saved(dr_ymm_t *xmm_caller_saved_buf);
 void
 get_ymm_caller_saved(dr_ymm_t *ymm_caller_saved_buf);
+void
+get_zmm_caller_saved(dr_zmm_t *zmm_caller_saved_buf);
 
 /* in encode.c */
 byte *

core/CMakeLists.txt
@@ -1059,6 +1059,11 @@ if (BUILD_TESTS)
     # -Wno-array-bounds to work around compiler bug (i#1796)
     set(unit_tests_extra_flags "${unit_tests_extra_flags} -Wno-array-bounds")
   endif ()
+  if (proc_supports_avx512)
+    set(unit_tests_extra_flags "${unit_tests_extra_flags} ${CFLAGS_AVX512}")
+  elseif (proc_supports_avx)
+    set(unit_tests_extra_flags "${unit_tests_extra_flags} ${CFLAGS_AVX}")
+  endif ()
   set_target_properties(unit_tests PROPERTIES
     COMPILE_FLAGS "-DRC_IS_TEST -DSTANDALONE_UNIT_TEST ${unit_tests_extra_flags}"
     RUNTIME_OUTPUT_DIRECTORY${location_suffix} "${EXECUTABLE_OUTPUT_DIRECTORY}")

commit 549bc6c964237797e6584942d46cf9eb2f45f847

i#1312 AVX-512 support: Add dr_mcontext_zmm_fields_valid() and lazy AVX-512 detection. (#3724)
Adds the function dr_mcontext_zmm_fields_valid() that returns true once AVX-512 has been
observed in main interp loop. The detection tries to be performance sensitive. Post-client
instructions are checked with a new function described below.

Adds the internal function instr_may_write_zmm_register() that conservatively returns whether
an instruction writes a zmm register.

Adds proc_avx512_enabled() and supports reading AVX-512 feature bits similar to
proc_avx_enabled().

Renames get_implied_mm_vex_opcode_bytes() to get_implied_mm_e_vex_opcode_bytes().

Adds a test for above covering the full decode case.

In an unrelated change, pulls in performance critical code into DO_ONCE in the decoder.

Fixes a cmake bug introduced in 6ca8d4a that set 0FF instead of OFF.

Issue: #1312

Match-id-6ff52d9d177382b4b5ecddaf1e0d74ad9f1b7731

CMakeLists.txt
@@ -1432,8 +1432,8 @@ if (BUILD_TESTS)
   # Tests require tools
   set(BUILD_TOOLS ON)
   enable_testing()
-  set(proc_supports_avx 0FF)
-  set(proc_supports_avx512 0FF)
+  set(proc_supports_avx OFF)
+  set(proc_supports_avx512 OFF)
   if (UNIX)
     set(CFLAGS_AVX "-mavx")
     set(CFLAGS_AVX512 "-mavx512f")

api/docs/release.dox
@@ -197,6 +197,9 @@ Further non-compatibility-affecting changes include:
  - Added the type #dr_encoding_hint_type_t.
  - Added #INSTR_ENCODING_HINT macro.
  - Added dr_standalone_exit() with support for re-attaching afterward.
+ - Added dr_mcontext_zmm_fields_valid() to query the state of lazy AVX-512 context
+   switching.
+ - Added the function proc_avx512_enabled().
 
 **************************************************
 <hr>

core/arch/arch.c
@@ -85,6 +85,10 @@ byte *app_sysenter_instr_addr = NULL;
 static bool sysenter_hook_failed = false;
 #endif
 
+#ifdef X86
+bool d_r_avx512_code_in_use = false;
+#endif
+
 /* static functions forward references */
 static byte *
 emit_ibl_routines(dcontext_t *dcontext, generated_code_t *code, byte *pc,

core/arch/arch.h
@@ -211,7 +211,7 @@ reg_spill_tls_offs(reg_id_t reg);
 #define REG_SAVED_XMM0 (YMM_ENABLED() ? REG_YMM0 : REG_XMM0)
 
 /* Xref the partially overlapping CONTEXT_PRESERVE_XMM */
-/* This routine also determines whether ymm registers should be saved */
+/* This routine also determines whether ymm registers should be saved. */
 static inline bool
 preserve_xmm_caller_saved(void)
 {
@@ -225,6 +225,26 @@ preserve_xmm_caller_saved(void)
      */
     return proc_has_feature(FEATURE_SSE) /* do xmm registers exist? */;
 }
+
+#ifdef X86
+/* This is used for AVX-512 context switching and indicates whether AVX-512 has been seen
+ * during decode.
+ */
+extern bool d_r_avx512_code_in_use;
+
+/* This routine determines whether zmm registers should be saved. */
+static inline bool
+d_r_is_avx512_code_in_use()
+{
+    return d_r_avx512_code_in_use;
+}
+
+static inline void
+d_r_set_avx512_code_in_use(bool in_use)
+{
+    d_r_avx512_code_in_use = in_use;
+}
+#endif
 
 typedef enum {
     IBL_UNLINKED,

core/arch/arch_exports.h
@@ -64,6 +64,7 @@
 #    define MCXT_TOTAL_SIMD_SLOTS_SIZE (MCXT_NUM_SIMD_SLOTS * MCXT_SIMD_SLOT_SIZE)
 /* Indicates OS support, not just processor support (xref i#1278) */
 #    define YMM_ENABLED() (proc_avx_enabled())
+#    define ZMM_ENABLED() (proc_avx512_enabled())
 #    define YMMH_REG_SIZE (YMM_REG_SIZE / 2) /* upper half */
 #    define MCXT_YMMH_SLOTS_SIZE (MCXT_NUM_SIMD_SLOTS * YMMH_REG_SIZE)
 #endif /* X86 */

core/arch/asm_defines.asm
@@ -328,6 +328,8 @@ ASSUME fs:_DATA @N@\
 #   define LIB_SEG_TLS gs /* keep in sync w/ unix/os_exports.h defines */
 #  endif
 # endif /* 64/32-bit */
+#  define REG_ZMM0 zmm0
+#  define REG_ZMM1 zmm1
 #endif /* ARM/X86 */
 
 /* calling convention */

core/arch/decode.h
@@ -83,6 +83,8 @@
  */
 #    define PREFIX_SEG_FS 0x20
 #    define PREFIX_SEG_GS 0x40
+/* Prefix used for AVX-512 */
+#    define PREFIX_EVEX 0x000100000
 #endif
 
 /* XXX: when adding prefixes, shift all the private values as they start

core/arch/instr.h
@@ -2383,6 +2383,16 @@ instr_writes_reg_list(instr_t *instr);
 #ifdef X86
 bool
 instr_can_set_single_step(instr_t *instr);
+
+/* Returns true if \p instr is part of Intel's AVX-512 instructions that may write to a
+ * zmm register. It approximates this by checking whether PREFIX_EVEX is set. If not set,
+ * it is looking at whether the instruction's raw bytes are valid, and if they are,
+ * whether the instruction is evex-encoded. The function assumes that the instruction's
+ * isa mode is set correctly. If the instruction's raw bytes are not valid, it checks the
+ * destinations of \p instr.
+ */
+bool
+instr_may_write_zmm_register(instr_t *instr);
 #endif
 
 DR_API

core/arch/interp.c
@@ -2833,6 +2833,15 @@ client_process_bb(dcontext_t *dcontext, build_bb_t *bb)
         if (!instr_opcode_valid(inst))
             continue;
 
+#    ifdef X86
+        if (!d_r_is_avx512_code_in_use()) {
+            if (ZMM_ENABLED()) {
+                if (instr_may_write_zmm_register(inst))
+                    d_r_set_avx512_code_in_use(true);
+            }
+        }
+#    endif
+
         if (instr_is_cti(inst) && inst != instrlist_last(bb->ilist)) {
             /* PR 213005: coarse_units can't handle added ctis (meta or not)
              * since decode_fragment(), used for state recreation, can't
@@ -3487,6 +3496,18 @@ build_bb_ilist(dcontext_t *dcontext, build_bb_t *bb)
             if (my_dcontext != NULL && debug_register_fire_on_addr(bb->instr_start)) {
                 stop_bb_on_fallthrough = true;
                 break;
+            }
+            if (!d_r_is_avx512_code_in_use()) {
+                if (ZMM_ENABLED()) {
+                    if (instr_get_prefix_flag(bb->instr, PREFIX_EVEX)) {
+                        /* For AVX-512 detection in bb builder, we're checking only for
+                         * the prefix flag, which for example can be set by decode_cti. In
+                         * client_process_bb, post-client instructions are checked with
+                         * instr_may_write_zmm_register.
+                         */
+                        d_r_set_avx512_code_in_use(true);
+                    }
+                }
             }
 #endif
             /* Eflags analysis:

core/arch/proc.h
@@ -262,6 +262,7 @@ typedef enum {
     FEATURE_ERMSB = 9 + 128,    /**< Enhanced rep movsb/stosb supported */
     FEATURE_INVPCID = 10 + 128, /**< #OP_invpcid supported */
     FEATURE_RTM = 11 + 128,     /**< Restricted Transactional Memory supported */
+    FEATURE_AVX512 = 16 + 128,  /**< AVX-512 instructions supported */
 } feature_bit_t;
 
 /**
@@ -294,6 +295,9 @@ extern size_t cache_line_size;
 
 /* xcr0 and xstate_bv feature bits */
 enum {
+    XCR0_HI16_ZMM = 7,
+    XCR0_ZMM_HI256 = 6,
+    XCR0_OPMASK = 5,
     XCR0_AVX = 4,
     XCR0_SSE = 2,
     XCR0_FP = 1,
@@ -550,5 +554,14 @@ DR_API
  */
 bool
 proc_avx_enabled(void);
+
+DR_API
+/**
+ * Returns whether AVX-512 is enabled by both the processor and the OS.
+ * Even if the processor supports AVX-512, if the OS does not enable AVX-512,
+ * then AVX-512 instructions will fault.
+ */
+bool
+proc_avx512_enabled(void);
 
 #endif /* _PROC_H_ */

core/arch/x86/decode.c
@@ -772,11 +772,13 @@ read_evex(byte *pc, decode_info_t *di, byte instr_byte,
         }
         *is_evex = true;
 #if !defined(STANDALONE_DECODER)
-        char pc_addr[IF_X64_ELSE(20, 12)];
-        snprintf(pc_addr, BUFFER_SIZE_ELEMENTS(pc_addr), PFX, pc);
-        NULL_TERMINATE_BUFFER(pc_addr);
-        DO_ONCE(SYSLOG(SYSLOG_ERROR, AVX_512_SUPPORT_INCOMPLETE, 2,
-                       get_application_name(), get_application_pid(), pc_addr));
+        DO_ONCE({
+            char pc_addr[IF_X64_ELSE(20, 12)];
+            snprintf(pc_addr, BUFFER_SIZE_ELEMENTS(pc_addr), PFX, pc);
+            NULL_TERMINATE_BUFFER(pc_addr);
+            SYSLOG(SYSLOG_ERROR, AVX_512_SUPPORT_INCOMPLETE, 2, get_application_name(),
+                   get_application_pid(), pc_addr);
+        });
 #endif
         info = &evex_prefix_extensions[0][1];
     } else {

core/arch/x86/decode_fast.c
@@ -560,8 +560,7 @@ decode_sizeof(dcontext_t *dcontext, byte *start_pc,
                          * and as such, while this will return the instruction's length,
                          * will still not be supported e.g. in decode_cti, because neither
                          * are there sizeof tables supporting prefix + > 1 opcode
-                         * instructions, nor do we support full decode of EVEX
-                         * instructions yet.
+                         * instructions.
                          */
                         vex_mm = (byte)(opc & 0x3);
                         opc = (uint) * (++pc); /* 3rd evex prefix byte */
@@ -1261,8 +1260,8 @@ intercept_fip_save(byte *pc, byte byte0, byte byte1)
 }
 
 static void
-get_implied_mm_vex_opcode_bytes(byte *pc, int prefixes, byte vex_mm, byte *byte0,
-                                byte *byte1)
+get_implied_mm_e_vex_opcode_bytes(byte *pc, int prefixes, byte vex_mm, byte *byte0,
+                                  byte *byte1)
 {
     switch (vex_mm) {
     case 1:
@@ -1278,7 +1277,7 @@ get_implied_mm_vex_opcode_bytes(byte *pc, int prefixes, byte vex_mm, byte *byte0
         *byte1 = 0x3a;
         break;
     default:
-        CLIENT_ASSERT(false, "get_implied_mm_vex_opcode_bytes: internal prefix error");
+        CLIENT_ASSERT(false, "get_implied_mm_e_vex_opcode_bytes: internal prefix error");
     }
 }
 
@@ -1353,13 +1352,15 @@ decode_cti(dcontext_t *dcontext, byte *pc, instr_t *instr)
                 byte1 = *(pc + prefixes);
                 break;
             case EVEX_PREFIX_OPCODE:
+                instr_set_prefix_flag(instr, PREFIX_EVEX);
+                /* fall-through */
             case VEX_3BYTE_PREFIX_OPCODE: {
                 /* EVEX and VEX 3-byte prefixes imply instruction opcodes by encoding mm
                  * bits in the second prefix byte. In theory, there are 5 VEX mm bits, but
                  * only 2 of them are used.
                  */
                 byte vex_mm = (byte)(*(pc + 1) & 0x3);
-                get_implied_mm_vex_opcode_bytes(pc, prefixes, vex_mm, &byte0, &byte1);
+                get_implied_mm_e_vex_opcode_bytes(pc, prefixes, vex_mm, &byte0, &byte1);
                 break;
             }
             default: break;

core/arch/x86/decode_private.h
@@ -70,8 +70,7 @@
 #define PREFIX_VEX_L 0x000040000
 /* Also only used during initial decode */
 #define PREFIX_XOP 0x000080000
-/* Prefixes which used for AVX-512 */
-#define PREFIX_EVEX 0x000100000
+/* Prefixes which are used for AVX-512 */
 #define PREFIX_EVEX_RR 0x000200000
 #define PREFIX_EVEX_LL 0x000400000
 #define PREFIX_EVEX_z 0x000800000

core/arch/x86/instr.c
@@ -1229,6 +1229,37 @@ instr_can_set_single_step(instr_t *instr)
 }
 
 bool
+instr_may_write_zmm_register(instr_t *instr)
+{
+    if (instr_get_prefix_flag(instr, PREFIX_EVEX))
+        return true;
+    if (instr_raw_bits_valid(instr)) {
+        /* At least 4 for evex bytes + 1 opcode byte. */
+        if (instr_length(NULL, instr) >= 5) {
+            if (instr_get_raw_byte(instr, 0) == EVEX_PREFIX_OPCODE) {
+                if (instr_get_isa_mode(instr) == DR_ISA_AMD64 ||
+                    /* If evex, P[3:2] must be 0 and P[10] must be 1. */
+                    TESTALL(MODRM_BYTE(3, 0, 0), instr_get_raw_byte(instr, 1))) {
+                    if (!TEST(0xC, instr_get_raw_byte(instr, 1)) &&
+                        TEST(0x04, instr_get_raw_byte(instr, 2))) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    for (int i = 0; i < instr_num_dsts(instr); ++i) {
+        opnd_t dst = instr_get_dst(instr, i);
+        if (opnd_is_reg(dst)) {
+            if (reg_is_strictly_zmm(opnd_get_reg(dst)))
+                return true;
+        }
+    }
+    return false;
+}
+
+bool
 instr_is_floating(instr_t *instr)
 {
     return instr_is_floating_ex(instr, NULL);

core/arch/x86/proc.c
@@ -68,6 +68,7 @@
 #define AMD_ECX /* cAMD */ 0x444d4163
 
 static bool avx_enabled;
+static bool avx512_enabled;
 
 static int num_simd_saved;
 static int num_simd_registers;
@@ -343,6 +344,8 @@ proc_init_arch(void)
             LOG(GLOBAL, LOG_TOP, 1, "\tProcessor has SSE3\n");
         if (proc_has_feature(FEATURE_AVX))
             LOG(GLOBAL, LOG_TOP, 1, "\tProcessor has AVX\n");
+        if (proc_has_feature(FEATURE_AVX512))
+            LOG(GLOBAL, LOG_TOP, 1, "\tProcessor has AVX-512\n");
         if (proc_has_feature(FEATURE_OSXSAVE))
             LOG(GLOBAL, LOG_TOP, 1, "\tProcessor has OSXSAVE\n");
     }
@@ -355,22 +358,39 @@ proc_init_arch(void)
     num_simd_saved = MCXT_NUM_SIMD_SLOTS;
     num_simd_registers = MCXT_NUM_SIMD_SLOTS;
 
-    if (proc_has_feature(FEATURE_AVX) && proc_has_feature(FEATURE_OSXSAVE)) {
-        /* Even if the processor supports AVX, it will #UD on any AVX instruction
-         * if the OS hasn't enabled YMM and XMM state saving.
-         * To check that, we invoke xgetbv -- for which we need FEATURE_OSXSAVE.
-         * FEATURE_OSXSAVE is also listed as one of the 3 steps in Intel Vol 1
-         * Fig 13-1: 1) cpuid OSXSAVE; 2) xgetbv 0x6; 3) cpuid AVX.
-         * Xref i#1278, i#1030, i#437.
-         */
+    if (proc_has_feature(FEATURE_OSXSAVE)) {
         uint bv_high = 0, bv_low = 0;
         dr_xgetbv(&bv_high, &bv_low);
-        LOG(GLOBAL, LOG_TOP, 2, "\txgetbv => 0x%08x%08x\n", bv_high, bv_low);
-        if (TESTALL(XCR0_AVX | XCR0_SSE, bv_low)) {
-            avx_enabled = true;
-            LOG(GLOBAL, LOG_TOP, 1, "\tProcessor and OS fully support AVX\n");
-        } else {
-            LOG(GLOBAL, LOG_TOP, 1, "\tOS does NOT support AVX\n");
+        if (proc_has_feature(FEATURE_AVX)) {
+            /* Even if the processor supports AVX, it will #UD on any AVX instruction
+             * if the OS hasn't enabled YMM and XMM state saving.
+             * To check that, we invoke xgetbv -- for which we need FEATURE_OSXSAVE.
+             * FEATURE_OSXSAVE is also listed as one of the 3 steps in Intel Vol 1
+             * Fig 13-1: 1) cpuid OSXSAVE; 2) xgetbv 0x6; 3) cpuid AVX.
+             * Xref i#1278, i#1030, i#437.
+             */
+            LOG(GLOBAL, LOG_TOP, 2, "\txgetbv => 0x%08x%08x\n", bv_high, bv_low);
+            if (TESTALL(XCR0_AVX | XCR0_SSE, bv_low)) {
+                avx_enabled = true;
+                LOG(GLOBAL, LOG_TOP, 1, "\tProcessor and OS fully support AVX\n");
+            } else {
+                LOG(GLOBAL, LOG_TOP, 1, "\tOS does NOT support AVX\n");
+            }
+        }
+        if (proc_has_feature(FEATURE_AVX512)) {
+            if (TESTALL(XCR0_HI16_ZMM | XCR0_ZMM_HI256 | XCR0_OPMASK, bv_low)) {
+                /* XXX i#1312: It had been unclear whether the kernel uses CR0 bits to
+                 * disable AVX-512 for its own lazy context switching optimization. If it
+                 * did, then our lazy context switch would interfere with the kernel's and
+                 * more support would be needed. We have concluded that the Linux kernel
+                 * at does not do its own lazy context switch optimization for AVX-512 at
+                 * this time.
+                 */
+                avx512_enabled = true;
+                LOG(GLOBAL, LOG_TOP, 1, "\tProcessor and OS fully support AVX-512\n");
+            } else {
+                LOG(GLOBAL, LOG_TOP, 1, "\tOS does NOT support AVX-512\n");
+            }
         }
     }
     for (i = 0; i < DEBUG_REGISTERS_NB; i++) {
@@ -543,4 +563,10 @@ bool
 proc_avx_enabled(void)
 {
     return avx_enabled;
+}
+
+bool
+proc_avx512_enabled(void)
+{
+    return avx512_enabled;
 }

core/lib/instrument.c
@@ -6452,6 +6452,16 @@ dr_mcontext_xmm_fields_valid(void)
     return preserve_xmm_caller_saved();
 }
 
+DR_API bool
+dr_mcontext_zmm_fields_valid(void)
+{
+#    ifdef X86
+    return d_r_is_avx512_code_in_use();
+#    else
+    return false;
+#    endif
+}
+
 #endif /* CLIENT_INTERFACE */
 /* dr_get_mcontext() needed for translating clean call arg errors */
 

core/lib/instrument_api.h
@@ -5565,14 +5565,26 @@ dr_clobber_retaddr_after_read(void *drcontext, instrlist_t *ilist, instr_t *inst
 
 DR_API
 /**
- * Returns true if the xmm fields in dr_mcontext_t are valid
+ * Returns true if the simd fields in dr_mcontext_t are valid xmm, ymm, or zmm values
  * (i.e., whether the underlying processor supports SSE).
  * \note If #DR_MC_MULTIMEDIA is not specified when calling dr_get_mcontext(),
- * the xmm fields will not be filled in regardless of the return value
+ * the simd fields will not be filled in regardless of the return value
  * of this routine.
  */
 bool
 dr_mcontext_xmm_fields_valid(void);
+
+DR_API
+/**
+ * Returns true if the simd fields in dr_mcontext_t are valid zmm values
+ * (i.e., whether the underlying processor and OS support AVX-512 and AVX-512 code
+ * is present).
+ * \note If #DR_MC_MULTIMEDIA is not specified when calling dr_get_mcontext(),
+ * the simd fields will not be filled in regardless of the return value
+ * of this routine.
+ */
+bool
+dr_mcontext_zmm_fields_valid(void);
 
 #endif /* CLIENT_INTERFACE */
 /* dr_get_mcontext() needed for translating clean call arg errors */

core/lib/mcxtx.h
@@ -229,12 +229,18 @@
          * Windows system calls.  These fields are ignored for 32-bit processes
          * that are not WOW64, or if the underlying processor does not support
          * SSE.  Use dr_mcontext_xmm_fields_valid() to determine whether the
-         * fields are valid.
+         * fields are valid. Use dr_mcontext_zmm_fields_valid() to determine
+         * whether zmm registers are preserved.
          *
-         * When the fields are valid, on processors with AVX enabled (i.e.,
-         * proc_has_feature(FEATURE_AVX) returns true), these fields will
+         * When the xmm fields are valid, on processors with AVX enabled (i.e.,
+         * proc_has_feature() with #FEATURE_AVX returns true), these fields will
          * contain the full ymm register values; otherwise, the top 128
          * bits of each slot will be undefined.
+         *
+         * When the zmm fields are valid, it implies that
+         * proc_has_feature() with #FEATURE_AVX512 is true. This is because DynamoRIO will
+         * not attempt to fill zmm fields w/o support by the processor and OS. The fields
+         * then will contain the full zmm register values.
          */
 #    ifdef AVOID_API_EXPORT
         /* PR 264138: we must preserve xmm0-5 if on a 64-bit Windows kernel,

suite/tests/CMakeLists.txt
@@ -1810,6 +1810,8 @@ if (CLIENT_INTERFACE)
       # For the subtest "targeting upper 2GB of low 4GB" we move -vm_base out of the
       # way:
       "-no_vm_base_near_app -vm_base 0x120000000" "")
+    # XXX i#1312, i#3504: The test doesn't currently compile with CFLAGS_AVX512 but
+    # attempts to test features based on it. The compile flag needs to be added.
     tobuild_ci(client.cleancall client-interface/cleancall.c "" "" "")
     if (UNIX)
       torunonly_ci(client.cleancall.prof-pcs client.cleancall client.cleancall.dll
@@ -2547,6 +2549,12 @@ endif ()
 
   if (NOT ANDROID) # XXX i#1874: get working on Android
     tobuild_ci(client.destructor client-interface/destructor.cpp "" "" "")
+  endif ()
+
+  if (proc_supports_avx512)
+    # XXX i#1312: This is untested on Windows.
+    tobuild_ci(client.avx512lazy client-interface/avx512lazy.c "" "" "")
+    set_target_properties(client.avx512lazy PROPERTIES COMPILE_FLAGS "${CFLAGS_AVX512}")
   endif ()
 
   if (BUILD_SAMPLES)

suite/tests/client-interface/avx512lazy.c
@@ -1,0 +1,119 @@
+/* **********************************************************
+ * Copyright (c) 2019 Google, Inc.  All rights reserved.
+ * **********************************************************/
+
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Google, Inc. nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL GOOGLE, INC. OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+#ifndef ASM_CODE_ONLY /* C code */
+#    include "tools.h"
+
+NOINLINE void
+before_marker();
+NOINLINE void
+after_marker();
+NOINLINE void
+avx512_instr();
+
+void
+run_avx512()
+{
+#    ifndef __AVX512F__
+#        error "Build error, should only be added with AVX-512 support."
+#    endif
+    before_marker();
+    /* DynamoRIO will detect AVX-512 during decode. We're making sure that the AVX-512
+     * instruction is not in the same basic block as the markers.
+     */
+    avx512_instr();
+    after_marker();
+    print("Ok\n");
+}
+
+int
+main()
+{
+    run_avx512();
+    return 0;
+}
+
+#else /* asm code *************************************************************/
+#    include "asm_defines.asm"
+/* clang-format off */
+START_FILE
+
+#ifdef X64
+# define FRAME_PADDING 0
+#else
+# define FRAME_PADDING 0
+#endif
+
+#define FUNCNAME before_marker
+        DECLARE_FUNC_SEH(FUNCNAME)
+GLOBAL_LABEL(FUNCNAME:)
+        sub      REG_XSP, FRAME_PADDING
+        END_PROLOG
+
+        mov      REG_XAX, 0x12345678
+        mov      REG_XAX, 0x12345678
+
+        add      REG_XSP, FRAME_PADDING
+        ret
+        END_FUNC(FUNCNAME)
+#undef FUNCNAME
+
+#define FUNCNAME after_marker
+        DECLARE_FUNC_SEH(FUNCNAME)
+GLOBAL_LABEL(FUNCNAME:)
+        sub      REG_XSP, FRAME_PADDING
+        END_PROLOG
+
+        mov      REG_XAX, 0x12345678
+        mov      REG_XAX, 0x12345678
+
+        add      REG_XSP, FRAME_PADDING
+        ret
+        END_FUNC(FUNCNAME)
+#undef FUNCNAME
+
+#define FUNCNAME avx512_instr
+        DECLARE_FUNC_SEH(FUNCNAME)
+GLOBAL_LABEL(FUNCNAME:)
+        sub      REG_XSP, FRAME_PADDING
+        END_PROLOG
+
+        vmovups  REG_ZMM0, REG_ZMM1
+
+        add      REG_XSP, FRAME_PADDING
+        ret
+        END_FUNC(FUNCNAME)
+#undef FUNCNAME
+
+END_FILE
+/* clang-format on */
+#endif

suite/tests/client-interface/avx512lazy.dll.c
@@ -1,0 +1,84 @@
+/* **********************************************************
+ * Copyright (c) 2019 Google, Inc.  All rights reserved.
+ * **********************************************************/
+
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Google, Inc. nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL GOOGLE, INC. OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+#include "dr_api.h"
+#include "client_tools.h"
+#include <string.h>
+
+#define CHECK(x, msg)                                                                \
+    do {                                                                             \
+        if (!(x)) {                                                                  \
+            dr_fprintf(STDERR, "CHECK failed %s:%d: %s\n", __FILE__, __LINE__, msg); \
+            dr_abort();                                                              \
+        }                                                                            \
+    } while (0);
+
+/* This library assumes a single-threaded test. */
+static bool before_seen;
+
+static dr_emit_flags_t
+bb_event(void *drcontext, void *tag, instrlist_t *bb, bool for_trace, bool translating)
+{
+    if (translating || for_trace)
+        return DR_EMIT_DEFAULT;
+    instr_t *instr;
+    bool prev_was_mov_const = false;
+    ptr_int_t val1, val2;
+    for (instr = instrlist_first(bb); instr != NULL; instr = instr_get_next(instr)) {
+        if (instr_is_mov_constant(instr, prev_was_mov_const ? &val2 : &val1)) {
+            if (prev_was_mov_const && val1 == val2 &&
+                val1 != 0 && /* rule out xor w/ self */
+                opnd_is_reg(instr_get_dst(instr, 0)) &&
+                opnd_get_reg(instr_get_dst(instr, 0)) == REG_XAX) {
+                if (before_seen) {
+                    CHECK(dr_mcontext_zmm_fields_valid(),
+                          "Error: dr_mcontext_zmm_fields_valid() should return true.");
+                    dr_fprintf(STDERR, "After\n");
+                } else {
+                    CHECK(!dr_mcontext_zmm_fields_valid(),
+                          "Error: dr_mcontext_zmm_fields_valid() should return false.");
+                    dr_fprintf(STDERR, "Before\n");
+                }
+                before_seen = true;
+            } else
+                prev_was_mov_const = true;
+        } else
+            prev_was_mov_const = false;
+    }
+    return DR_EMIT_DEFAULT;
+}
+
+DR_EXPORT void
+dr_init(client_id_t id)
+{
+    dr_register_bb_event(bb_event);
+}

suite/tests/client-interface/avx512lazy.templatex
@@ -1,0 +1,4 @@
+Before
+(<Application .*client\.avx512lazy.*AVX-512 was detected at PC 0x[0-9a-f]+. AVX-512 is not fully supported yet.>
+)?After
+Ok