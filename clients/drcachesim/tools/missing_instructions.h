821d84aa44b4aa07b4117654159128ee52db9743
Edin Jelacic <jelacic.edin@gmail.com>
2024-01-26 16:04:51 +0100

    Modifications to enable db

--- clients/drcachesim/tools/missing_instructions.h
+++ clients/drcachesim/tools/missing_instructions.h
@@ -54,35 +54,38 @@ public:
 
     bool
     get_opcode(const memref_t &memref);
-  
+
     missing_instructions_t(const cache_simulator_knobs_t &knobs);
-    // missing_instructions_t(const std::string &module_file_path, memref_tid_t thread, uint64_t skip_refs,
-           // uint64_t sim_refs, const std::string &syntax, unsigned int verbose,
-           // const std::string &alt_module_dir = "");
-//     std::string
-//     initialize_stream(memtrace_stream_t *serial_stream) override;
-//     bool
-//     parallel_shard_supported() override;
-//     void *
-//     parallel_shard_init_stream(int shard_index, void *worker_data,
-//                                memtrace_stream_t *shard_stream) override;
-//     bool
-//     parallel_shard_exit(void *shard_data) override;
-//     bool
-//     parallel_shard_memref(void *shard_data, const memref_t &memref) override;
-//     std::string
-//     parallel_shard_error(void *shard_data) override;
+    // missing_instructions_t(const std::string &module_file_path, memref_tid_t thread,
+    // uint64_t skip_refs, uint64_t sim_refs, const std::string &syntax, unsigned int
+    // verbose, const std::string &alt_module_dir = "");
+    //     std::string
+    //     initialize_stream(memtrace_stream_t *serial_stream) override;
+    //     bool
+    //     parallel_shard_supported() override;
+    //     void *
+    //     parallel_shard_init_stream(int shard_index, void *worker_data,
+    //                                memtrace_stream_t *shard_stream) override;
+    //     bool
+    //     parallel_shard_exit(void *shard_data) override;
+    //     bool
+    //     parallel_shard_memref(void *shard_data, const memref_t &memref) override;
+    //     std::string
+    //     parallel_shard_error(void *shard_data) override;
     bool
     process_memref(const memref_t &memref) override;
     bool
     print_results() override;
-    void print_instr_stats(int core);
+    void
+    print_instr_stats(int core, bool thread_switch, bool core_switch, const memref_t &memref);
+    bool
+    print_miss_stats_and_run_cache_instr_sim(int core, const memref_t &memref);
 
-//     static constexpr int
-//     tid_column_width()
-//     {
-//         return TID_COLUMN_WIDTH;
-//     }
+    //     static constexpr int
+    //     tid_column_width()
+    //     {
+    //         return TID_COLUMN_WIDTH;
+    //     }
 
 protected:
     struct dcontext_cleanup_last_t {
@@ -96,79 +99,82 @@ protected:
     };
 
     // bool
-//     should_skip(memtrace_stream_t *memstream, const memref_t &memref);
+    //     should_skip(memtrace_stream_t *memstream, const memref_t &memref);
 
-//     inline void
-//     print_header()
-//     {
-//         std::cerr << "Output format:\n"
-//                   << "<--record#-> <--instr#->: <---tid---> <record details>\n"
-//                   << "------------------------------------------------------------\n";
-//     }
+    //     inline void
+    //     print_header()
+    //     {
+    //         std::cerr << "Output format:\n"
+    //                   << "<--record#-> <--instr#->: <---tid---> <record details>\n"
+    //                   <<
+    //                   "------------------------------------------------------------\n";
+    //     }
 
-//     inline void
-//     print_prefix(memtrace_stream_t *memstream, const memref_t &memref,
-//                  int64_t record_ord_subst = -1, std::ostream &stream = std::cerr)
-//     {
-//         uint64_t record_ord = record_ord_subst > -1
-//             ? static_cast<uint64_t>(record_ord_subst)
-//             : memstream->get_record_ordinal();
-//         if ((prev_tid_ != -1 && prev_tid_ != memref.instr.tid) ||
-//             // Print a divider for a skip_instructions gap too.
-//             (prev_record_ != 0 && prev_record_ + 1 < record_ord))
-//             stream << "------------------------------------------------------------\n";
-//         prev_tid_ = memref.instr.tid;
-//         prev_record_ = record_ord;
+    //     inline void
+    //     print_prefix(memtrace_stream_t *memstream, const memref_t &memref,
+    //                  int64_t record_ord_subst = -1, std::ostream &stream = std::cerr)
+    //     {
+    //         uint64_t record_ord = record_ord_subst > -1
+    //             ? static_cast<uint64_t>(record_ord_subst)
+    //             : memstream->get_record_ordinal();
+    //         if ((prev_tid_ != -1 && prev_tid_ != memref.instr.tid) ||
+    //             // Print a divider for a skip_instructions gap too.
+    //             (prev_record_ != 0 && prev_record_ + 1 < record_ord))
+    //             stream <<
+    //             "------------------------------------------------------------\n";
+    //         prev_tid_ = memref.instr.tid;
+    //         prev_record_ = record_ord;
 
-//         stream << std::setw(RECORD_COLUMN_WIDTH) << record_ord
-//                << std::setw(INSTR_COLUMN_WIDTH) << memstream->get_instruction_ordinal()
-//                << ": " << std::setw(TID_COLUMN_WIDTH) << memref.marker.tid << " ";
-//     }
+    //         stream << std::setw(RECORD_COLUMN_WIDTH) << record_ord
+    //                << std::setw(INSTR_COLUMN_WIDTH) <<
+    //                memstream->get_instruction_ordinal()
+    //                << ": " << std::setw(TID_COLUMN_WIDTH) << memref.marker.tid << " ";
+    //     }
 
-//     /* We make this the first field so that dr_standalone_exit() is called after
-//      * destroying the other fields which may use DR heap.
-//      */
+    //     /* We make this the first field so that dr_standalone_exit() is called after
+    //      * destroying the other fields which may use DR heap.
+    //      */
     dcontext_cleanup_last_t dcontext_;
 
-//     // These are all optional and unused for OFFLINE_FILE_TYPE_ENCODINGS.
-//     // XXX: Once we update our toolchains to guarantee C++17 support we could use
-//     // std::optional here.
-//     std::string module_file_path_;
-//     std::unique_ptr<module_mapper_t> module_mapper_;
-//     raw2trace_directory_t directory_;
+    //     // These are all optional and unused for OFFLINE_FILE_TYPE_ENCODINGS.
+    //     // XXX: Once we update our toolchains to guarantee C++17 support we could use
+    //     // std::optional here.
+    //     std::string module_file_path_;
+    //     std::unique_ptr<module_mapper_t> module_mapper_;
+    //     raw2trace_directory_t directory_;
 
-//     unsigned int knob_verbose_;
-//     int trace_version_;
+    //     unsigned int knob_verbose_;
+    //     int trace_version_;
     static const std::string TOOL_NAME;
-//     memref_tid_t knob_thread_;
-//     uint64_t knob_skip_refs_;
-//     uint64_t skip_refs_left_;
-//     uint64_t knob_sim_refs_;
-//     uint64_t sim_refs_left_;
-//     bool refs_limited_;
-//     std::string knob_syntax_;
-//     std::string knob_alt_module_dir_;
-//     uint64_t num_disasm_instrs_;
-//     std::unordered_map<app_pc, std::string> disasm_cache_;
-//     memref_tid_t prev_tid_;
-//     uint64_t prev_record_ = 0;
-//     intptr_t filetype_;
-//     std::unordered_set<memref_tid_t> printed_header_;
-//     std::unordered_map<memref_tid_t, uintptr_t> last_window_;
-//     uintptr_t timestamp_;
-//     int64_t timestamp_record_ord_ = -1;
-//     int64_t version_record_ord_ = -1;
-//     int64_t filetype_record_ord_ = -1;
-//     bool has_modules_;
-//     memtrace_stream_t *serial_stream_ = nullptr;
+    //     memref_tid_t knob_thread_;
+    //     uint64_t knob_skip_refs_;
+    //     uint64_t skip_refs_left_;
+    //     uint64_t knob_sim_refs_;
+    //     uint64_t sim_refs_left_;
+    //     bool refs_limited_;
+    //     std::string knob_syntax_;
+    //     std::string knob_alt_module_dir_;
+    //     uint64_t num_disasm_instrs_;
+    //     std::unordered_map<app_pc, std::string> disasm_cache_;
+    //     memref_tid_t prev_tid_;
+    //     uint64_t prev_record_ = 0;
+    //     intptr_t filetype_;
+    //     std::unordered_set<memref_tid_t> printed_header_;
+    //     std::unordered_map<memref_tid_t, uintptr_t> last_window_;
+    //     uintptr_t timestamp_;
+    //     int64_t timestamp_record_ord_ = -1;
+    //     int64_t version_record_ord_ = -1;
+    //     int64_t filetype_record_ord_ = -1;
+    //     bool has_modules_;
+    //     memtrace_stream_t *serial_stream_ = nullptr;
 
 private:
-  int current_instruction_id = 0;
-//     static constexpr int RECORD_COLUMN_WIDTH = 12;
-//     static constexpr int INSTR_COLUMN_WIDTH = 12;
-//     static constexpr int TID_COLUMN_WIDTH = 11;
-  uintptr_t curr_core_id;
-  memref_tid_t curr_thread_id;
+    int current_instruction_id = 0;
+    //     static constexpr int RECORD_COLUMN_WIDTH = 12;
+    //     static constexpr int INSTR_COLUMN_WIDTH = 12;
+    //     static constexpr int TID_COLUMN_WIDTH = 11;
+    uintptr_t curr_core_id;
+    memref_tid_t curr_thread_id;
 };
 
 #endif /* _MISSING_INSTRUCTIONS_H_ */
